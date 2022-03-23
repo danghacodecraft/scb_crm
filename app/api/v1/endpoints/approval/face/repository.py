202bbe7e498bcb325ebc956052d309b9fc1c0e5f
aitcq <aitcq@scb.com.vn>
2022-03-23 16:59:57 +0700

    [refactor](#104): face for approve

--- app/api/v1/endpoints/approval/face/repository.py
+++ app/api/v1/endpoints/approval/face/repository.py
@@ -1,13 +1,17 @@
-from sqlalchemy import desc, select
+from typing import List
+
+from sqlalchemy import and_, desc, select
 from sqlalchemy.orm import Session
 
-from app.api.base.repository import ReposReturn
+from app.api.base.repository import ReposReturn, auto_commit
 from app.third_parties.oracle.models.cif.basic_information.identity.model import (
-    CustomerCompareImage, CustomerCompareImageTransaction, CustomerIdentity
+    CustomerCompareImage, CustomerCompareImageTransaction, CustomerIdentity,
+    CustomerIdentityImage
 )
 from app.third_parties.oracle.models.cif.basic_information.model import (
     Customer
 )
+from app.utils.constant.cif import IMAGE_TYPE_FACE
 
 
 async def repos_get_approval_compare_faces(
@@ -16,31 +20,48 @@ async def repos_get_approval_compare_faces(
 ):
     """
     Lấy tất cả hình ảnh ở bước GTDD
+    Output: Customer, CustomerIdentity, CustomerIdentityImage, CustomerCompareImage, CustomerCompareImageTransaction
     """
     customer_identities = session.execute(
         select(
             Customer,
             CustomerIdentity,
+            CustomerIdentityImage,
+            # CustomerCompareImage,
+            # CustomerCompareImageTransaction
         )
         .join(CustomerIdentity, Customer.id == CustomerIdentity.customer_id)
+        .join(CustomerIdentityImage, and_(
+            CustomerIdentity.id == CustomerIdentityImage.identity_id,
+            CustomerIdentityImage.image_type_id == IMAGE_TYPE_FACE
+        ))
         .filter(Customer.id == cif_id)
         .order_by(desc(CustomerIdentity.updater_at))
-    ).first()
-
-    _, customer_identity = customer_identities
-    customer_identity_id = customer_identity.id
-    face_compares = session.execute(
-        select(
-            CustomerCompareImage,
-            CustomerCompareImageTransaction
-        )
-        .join(CustomerCompareImageTransaction,
-              CustomerCompareImage.id == CustomerCompareImageTransaction.compare_image_id)
-        .filter(CustomerCompareImage.identity_id == customer_identity_id)
-        .order_by(desc(CustomerCompareImageTransaction.maker_at))
     ).all()
-
-    if not face_compares:
+    if not customer_identities:
         return ReposReturn(is_error=True, detail="No Face in Identity Step")
 
-    return ReposReturn(data=face_compares)
+    return ReposReturn(data=customer_identities)
+
+
+@auto_commit
+async def repos_save_approval_compare_face(
+        saving_customer_compare_images: List,
+        saving_customer_compare_image_transactions: List,
+        session: Session
+):
+    """
+    Lưu hình ảnh So sánh ở bước Phê duyệt
+    """
+
+    session.bulk_save_objects([
+        CustomerCompareImage(**customer_compare_image)
+        for customer_compare_image in saving_customer_compare_images
+    ])
+    session.bulk_save_objects([
+        CustomerCompareImageTransaction(**customer_compare_image_transaction)
+        for customer_compare_image_transaction in saving_customer_compare_image_transactions
+    ])
+    session.flush()
+
+    return ReposReturn(data=None)
