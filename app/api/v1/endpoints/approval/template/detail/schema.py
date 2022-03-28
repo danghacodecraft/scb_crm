571d2366ec57584f07da33dc27762fdca1675620
aitcq <aitcq@scb.com.vn>
2022-03-28 20:21:27 +0700

    [refactor]: Approval -> Template -> Detail -- detached module

--- app/api/v1/endpoints/approval/template/detail/schema.py
+++ app/api/v1/endpoints/approval/template/detail/schema.py
@@ -20,25 +20,3 @@ class ProcessInfoResponse(BaseSchema):
 class CifApprovalProcessResponse(BaseSchema):
     created_at: date = Field(..., description="Ngày tạo")
     logs: List[ProcessInfoResponse] = Field(..., description="Danh sách log trong 1 ngày ")
-
-
-class CifApprovalResponse(BaseSchema):
-    cif_id: str = Field(..., description="Cif ID")
-    previous_stage: Optional[str] = Field(..., description="Bước trước đó")
-    current_stage: str = Field(..., description="Bước hiện tại")
-    next_stage: str = Field(..., description="Bước tiếp theo")
-
-
-class CIFStageResponse(BaseSchema):
-    stage_code: Optional[str] = Field(..., description="Mã bước giao dịch")
-    is_disable: bool = Field(..., description="Có disable không")
-    is_completed: bool = Field(..., description="Trạng thái phê duyệt")
-    content: Optional[str] = Field(..., description="1. Nội dung phản hồi")
-    action: Optional[str] = Field(None, description="2. Hành động")
-    created_at: Optional[datetime] = Field(..., description="Cập nhật lúc")
-    created_by: Optional[str] = Field(..., description="Cập nhật bởi")
-
-
-class CifApprovalSuccessResponse(BaseSchema):
-    cif_id: str = Field(..., description="Cif ID")
-    stages: List[CIFStageResponse] = Field(..., description="Thông tin các bước phê duyệt")
