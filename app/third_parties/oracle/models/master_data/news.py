4cae46d620e3f177c8e7047ad424c70e4bdd49b2
aitcq <aitcq@scb.com.vn>
2022-03-30 11:30:56 +0700

    [add]: News Model

---
+++ app/third_parties/oracle/models/master_data/news.py
@@ -0,0 +1,15 @@
+from sqlalchemy import VARCHAR, Column, text
+from sqlalchemy.dialects.oracle import NUMBER
+
+from app.third_parties.oracle.base import Base
+
+
+class NewsCategory(Base):
+    __tablename__ = 'crm_news'
+
+    id = Column('news_category_id', VARCHAR(36), primary_key=True, server_default=text("sys_guid() "),
+                comment='ID Mã chuyên mục')
+    code = Column('news_category_code', VARCHAR(50), nullable=False, comment='Mã chuyên mục')
+    name = Column('news_category_name', VARCHAR(255), nullable=False, comment='Tên chuyên mục')
+    active_flag = Column('active_flag', NUMBER(1, 0, False), nullable=False, comment='Trạng thái hoạt động')
+    order_no = Column('order_no', NUMBER(3, 0, False), comment='Sắp xếp')
