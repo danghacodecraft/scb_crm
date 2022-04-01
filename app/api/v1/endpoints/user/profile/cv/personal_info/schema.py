fd67aa60f79c654b8c7e780e9a9c92fbbbb8a933
lebadai <30581741+lebadai@users.noreply.github.com>
2022-04-01 11:38:15 +0700

    [add]: personal info

--- app/api/v1/endpoints/user/profile/cv/personal_info/schema.py
+++ app/api/v1/endpoints/user/profile/cv/personal_info/schema.py
@@ -1,21 +1,22 @@
 from datetime import date
+from typing import Optional
 
 from pydantic import Field
 
 from app.api.base.schema import BaseSchema
+from app.api.v1.schemas.utils import OptionalDropdownResponse
+
+
 # Thông tin cá nhân
-from app.api.v1.schemas.utils import DropdownResponse
-
-
 class PersonalInfoResponse(BaseSchema):
-    date_of_birth: date = Field(..., description="Ngày sinh")
-    place_of_birth: DropdownResponse = Field(..., description="Nơi sinh")
-    gender: DropdownResponse = Field(..., description="Giới tính")
-    ethnic: str = Field(..., description="Dân tộc")
-    religion: str = Field(..., description="Tôn giáo")
-    nationality: DropdownResponse = Field(..., description="Quốc tịch")
-    marital_status: bool = Field(..., description="Tình trạng hôn nhân")
-    identity_num: str = Field(..., description="Số CMND/CCCD/Hộ chiếu")
-    issued_date: date = Field(..., description="Ngày cấp")
-    expiration_date: date = Field(None, description="Ngày hết hạn")
-    place_of_issue: DropdownResponse = Field(..., description="Nơi cấp")
+    date_of_birth: Optional[date] = Field(..., description="Ngày sinh")
+    place_of_birth: OptionalDropdownResponse = Field(..., description="Nơi sinh")
+    gender: OptionalDropdownResponse = Field(..., description="Giới tính")
+    ethnic: OptionalDropdownResponse = Field(..., description="Dân tộc")
+    religion: OptionalDropdownResponse = Field(..., description="Tôn giáo")
+    nationality: OptionalDropdownResponse = Field(..., description="Quốc tịch")
+    marital_status: OptionalDropdownResponse = Field(..., description="Tình trạng hôn nhân")
+    identity_number: Optional[str] = Field(..., description="Số CMND/CCCD/Hộ chiếu")
+    issued_date: Optional[date] = Field(..., description="Ngày cấp")
+    expired_date: Optional[date] = Field(None, description="Ngày hết hạn")
+    place_of_issue: OptionalDropdownResponse = Field(..., description="Nơi cấp")
