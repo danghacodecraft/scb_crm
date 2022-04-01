28d32a0e603e8561559f6fa9115457116d8b940c
lebadai <30581741+lebadai@users.noreply.github.com>
2022-04-01 14:45:26 +0700

    [add]: it

--- app/api/v1/endpoints/user/profile/level/it/schema.py
+++ app/api/v1/endpoints/user/profile/level/it/schema.py
@@ -7,6 +7,6 @@ from app.api.base.schema import BaseSchema
 
 # Thông tin trình độ tin học
 class ITLevelInfoResponse(BaseSchema):
-    certification: Optional[str] = Field(..., description="Chứng chỉ"),
+    certification: Optional[str] = Field(..., description="Chứng chỉ")
     level: Optional[str] = Field(..., description="Trình độ")
     gpa: Optional[str] = Field(None, description="Điểm số/Xếp loại")
