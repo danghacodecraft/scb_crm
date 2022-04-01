d2f35f83c6c9c91c88bbbd424ba569ac148ba2f8
lebadai <30581741+lebadai@users.noreply.github.com>
2022-04-01 11:58:59 +0700

    [add]: language info

--- app/api/v1/endpoints/user/profile/level/foreign/schema.py
+++ app/api/v1/endpoints/user/profile/level/foreign/schema.py
@@ -1,14 +1,15 @@
 from datetime import date
+from typing import Optional
 
 from pydantic import Field
 
 from app.api.base.schema import BaseSchema
-from app.api.v1.schemas.utils import DropdownResponse
+from app.api.v1.schemas.utils import OptionalDropdownResponse
 
 
 # Thông tin trình độ ngoại ngữ
 class ForeignLanguageLevelInfoResponse(BaseSchema):
-    foreign_language: str = Field(..., description="Ngoại ngữ")
-    level: DropdownResponse = Field(..., description="Trình độ")
-    point: str = Field(None, description="Điểm")
-    certificate_date: date = Field(None, description="Ngày nhận chứng chỉ")
+    language_type: Optional[str] = Field(..., description="Ngoại ngữ")
+    level: OptionalDropdownResponse = Field(..., description="Trình độ")
+    point: Optional[str] = Field(None, description="Điểm")
+    certification_date: Optional[date] = Field(None, description="Ngày nhận chứng chỉ")
