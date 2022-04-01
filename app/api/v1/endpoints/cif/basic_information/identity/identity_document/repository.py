5cc1482e7ac6249265e120ae2467703ff1a9c05b
Phạm Thành Đạt <datpt@minerva.vn>
2022-04-01 11:42:02 +0700

    [hard] hard for debug

--- app/api/v1/endpoints/cif/basic_information/identity/identity_document/repository.py
+++ app/api/v1/endpoints/cif/basic_information/identity/identity_document/repository.py
@@ -734,8 +734,11 @@ async def repos_upload_identity_document_and_ocr(
         return ReposReturn(is_error=True, msg=ERROR_CALL_SERVICE_EKYC, detail=ocr_response.get('message', ''))
 
     file_response = await service_file.upload_file(file=image_file, name=image_file_name)
+    
     if not file_response:
-        return ReposReturn(is_error=True, msg=ERROR_CALL_SERVICE_FILE, detail=file_response)
+        return {
+            'file_res': file_response
+        }
 
     if identity_type == EKYC_IDENTITY_TYPE_FRONT_SIDE_IDENTITY_CARD:
         response_data = await mapping_ekyc_front_side_identity_card_ocr_data(
