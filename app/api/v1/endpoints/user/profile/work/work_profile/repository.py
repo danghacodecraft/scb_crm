a803e6c64fc6e05befcfc41839acb281c9f3be26
lebadai <30581741+lebadai@users.noreply.github.com>
2022-04-01 15:11:15 +0700

    [add]: work profile

--- app/api/v1/endpoints/user/profile/work/work_profile/repository.py
+++ app/api/v1/endpoints/user/profile/work/work_profile/repository.py
@@ -7,47 +7,47 @@ async def repos_work_profile(
     session: Session
 ) -> ReposReturn:
     data_response = {
-        "working_day": "06/06/2021",
-        "probationary_day": "06/09/2021",
-        "official_date": "15/09/2021",
-        "current_unit": {
-            "current_working_unit": {
+        "working_date": "2021-06-06",
+        "probationary_date": "2021-06-09",
+        "official_date": "2021-06-19",
+        "current": {
+            "branch": {
                 "id": "1",
                 "code": "Code",
                 "name": "Phòng quản lý khai thác, phân tích dữ liệu"
             },
-            "company_position": {
+            "position": {
                 "id": "1",
                 "code": "Code",
                 "name": "Nhân viên"
             }
         },
-        "root_unit": {
-            "original_unit": {
+        "root": {
+            "branch": {
                 "id": "1",
                 "code": "Code",
                 "name": "Phòng quản lý khai thác, phân tích dữ liệu"
             },
-            "company_position": {
+            "position": {
                 "id": "1",
                 "code": "Code",
                 "name": "Nhân viên"
             }
         },
-        "temporary_unit": {
-            "temporary_unit": {
+        "temporary": {
+            "branch": {
                 "id": "1",
                 "code": "Code",
                 "name": "Phòng quản lý khai thác, phân tích dữ liệu"
             },
-            "company_position": {
+            "position": {
                 "id": "1",
                 "code": "Code",
                 "name": "Nhân viên"
             }
         },
         "seniority_date": "2021-09-06",
-        "resident_object": True
+        "is_resident": True
     }
 
     return ReposReturn(data=data_response)
