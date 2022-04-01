96f19bab450a0afdde41283f13545a07395713e9
lebadai <30581741+lebadai@users.noreply.github.com>
2022-04-01 13:35:29 +0700

    [add]: discipline

--- app/api/v1/endpoints/user/profile/other/discipline/schema.py
+++ app/api/v1/endpoints/user/profile/other/discipline/schema.py
@@ -1,4 +1,5 @@
 from datetime import date
+from typing import Optional
 
 from pydantic import Field
 
@@ -6,16 +7,16 @@ from app.api.base.schema import BaseSchema
 
 
 # Quá trình kỷ luật
-class DiscriplineResponse(BaseSchema):
-    effective_date: date = Field(None, description="Ngày hiệu lực")
-    end_date: date = Field(None, description="Ngày kết thúc")
-    titles: str = Field(None, description="Chức danh")
-    dep_name: str = Field(None, description="Đơn vị/Phòng ban")
-    disciplinary_reasons: str = Field(None, description="Lý do kỷ luật")
-    detailed_reason: str = Field(None, description="Lý do chi tiết")
-    date_detect: date = Field(None, description="Ngày phát hiện")
-    date_violation: date = Field(None, description="Ngày vi phạm")
-    total_damage_value: str = Field(None, description="Tổng giá trị thiệt hại")
-    decision_number: str = Field(None, description="Số quyết định")
-    people_delete_discipline: str = Field(None, description="Người xóa kỷ luật")
-    signer: str = Field(None, description="Người ký")
+class DisciplineResponse(BaseSchema):
+    effective_date: Optional[date] = Field(None, description="Ngày hiệu lực")
+    end_date: Optional[date] = Field(None, description="Ngày kết thúc")
+    titles: Optional[str] = Field(None, description="Chức danh")
+    department: Optional[str] = Field(None, description="Đơn vị/Phòng ban")
+    reasons: Optional[str] = Field(None, description="Lý do kỷ luật")
+    detailed_reason: Optional[str] = Field(None, description="Lý do chi tiết")
+    detected_date: Optional[str] = Field(None, description="Ngày phát hiện")
+    violation_date: Optional[str] = Field(None, description="Ngày vi phạm")
+    total_damage: Optional[str] = Field(None, description="Tổng giá trị thiệt hại")
+    number: Optional[str] = Field(None, description="Số quyết định")
+    deleter: Optional[str] = Field(None, description="Người xóa kỷ luật")
+    signer: Optional[str] = Field(None, description="Người ký")
--- app/api/v1/endpoints/user/profile/other/discipline/view.py
+++ app/api/v1/endpoints/user/profile/other/discipline/view.py
@@ -1,13 +1,15 @@
+from typing import List
+
 from fastapi import APIRouter
 from starlette import status
 
-from app.api.base.schema import PagingResponse
+from app.api.base.schema import ResponseData
 from app.api.base.swagger import swagger_response
 from app.api.v1.endpoints.user.profile.other.discipline.controller import (
     CtrDiscipline
 )
 from app.api.v1.endpoints.user.profile.other.discipline.schema import (
-    DiscriplineResponse
+    DisciplineResponse
 )
 
 router = APIRouter()
@@ -18,7 +20,7 @@ router = APIRouter()
     name="[THÔNG TIN KHÁC] - B. KỶ LUẬT",
     description="[THÔNG TIN KHÁC] - B. KỶ LUẬT",
     responses=swagger_response(
-        response_model=PagingResponse[DiscriplineResponse],
+        response_model=ResponseData[List[DisciplineResponse]],
         success_status_code=status.HTTP_200_OK
     )
 )
@@ -26,4 +28,4 @@ async def view_retrieve_current_user(
         # current_user=Depends(get_current_user_from_header())
 ):
     user_info = await CtrDiscipline().ctr_discipline()
-    return PagingResponse[DiscriplineResponse](**user_info)
+    return ResponseData[List[DisciplineResponse]](**user_info)
