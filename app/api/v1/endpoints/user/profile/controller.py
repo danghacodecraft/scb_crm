88c0f83e299efd751d082abd8dfaf7250072cc4a
lebadai <30581741+lebadai@users.noreply.github.com>
2022-04-01 16:23:47 +0700

    [add]: service_dwh

--- app/api/v1/endpoints/user/profile/controller.py
+++ app/api/v1/endpoints/user/profile/controller.py
@@ -1,15 +1,17 @@
 from app.api.base.controller import BaseController
 from app.api.v1.endpoints.user.profile.repository import repos_profile
-from app.settings.event import service_dwh
 
 
 class CtrProfile(BaseController):
     async def ctr_profile(self, employee_id: str):
-        is_success, data = await service_dwh.detail(employee_id=employee_id)
-        profile = self.call_repos(
+
+        is_success, profile = self.call_repos(
             await repos_profile(
+                employee_id=employee_id,
                 session=self.oracle_session
             )
         )
+        if not is_success:
+            self.response_exception(msg=str(profile))
 
         return self.response(data=profile)
--- app/api/v1/endpoints/user/profile/repository.py
+++ app/api/v1/endpoints/user/profile/repository.py
@@ -1,20 +1,23 @@
 from sqlalchemy.orm import Session
 
 from app.api.base.repository import ReposReturn
+from app.settings.event import service_dwh
 
 
 async def repos_profile(
+        employee_id: str,
         session: Session
 ) -> ReposReturn:
-    data_reponse = {
-        "full_name_vn": "NGUYỄN THỊ PHƯƠNG THẢO",
-        "email": "nguyenvana@scb.vn",
-        "mobile_number": "(+84)896 524 256",
-        "code": "1234",
-        "department": "B0-Phòng chung khối doanh nghiệp",
-        "titles": "Phó giám đốc khối kinh doanh",
-        "manager": "123456-Nguyễn Văn A",
-        "telephone_number": "(+28) 123 456 789"
-    }
+    data_reponse = await service_dwh.detail(employee_id=employee_id)
+    # data_reponse = {
+    #     "full_name_vn": "NGUYỄN THỊ PHƯƠNG THẢO",
+    #     "email": "nguyenvana@scb.vn",
+    #     "mobile_number": "(+84)896 524 256",
+    #     "code": "1234",
+    #     "department": "B0-Phòng chung khối doanh nghiệp",
+    #     "titles": "Phó giám đốc khối kinh doanh",
+    #     "manager": "123456-Nguyễn Văn A",
+    #     "telephone_number": "(+28) 123 456 789"
+    # }
 
     return ReposReturn(data=data_reponse)
