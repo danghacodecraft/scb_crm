be534a9387b151259fde8b782b182838ccbb7257
lebadai <30581741+lebadai@users.noreply.github.com>
2022-04-01 13:30:13 +0700

    [add]: felicition

--- app/api/v1/endpoints/user/profile/other/felicitation/schema.py
+++ app/api/v1/endpoints/user/profile/other/felicitation/schema.py
@@ -1,17 +1,19 @@
+from typing import Optional
+
 from pydantic import Field
 
 from app.api.base.schema import BaseSchema
 
 
 class FelicitionResponse(BaseSchema):
-    effective_date: str = Field(None, description="Ngày hiệu lực")
-    decision_number: str = Field(None, description="Số quyết định")
-    titles: str = Field(None, description="Danh hiệu")
-    rank_commend: str = Field(None, description="Cấp khen thưởng")
-    job_title_name: str = Field(None, description="Chức danh")
-    dep_name: str = Field(None, description="Đơn vị/Phòng ban")
-    reason_commend: str = Field(None, description="Lý do khen thưởng")
-    form_commend: str = Field(None, description="Hình thức khen thưởng")
-    bonus_amount: str = Field(None, description="Số tiền khen thưởng")
-    sign_day: str = Field(None, description="Ngày ký")
-    signer: str = Field(None, description="Người ký")
+    effective_date: Optional[str] = Field(None, description="Ngày hiệu lực")
+    decision_number: Optional[str] = Field(None, description="Số quyết định")
+    titles: Optional[str] = Field(None, description="Danh hiệu")
+    commend_level: Optional[str] = Field(None, description="Cấp khen thưởng")
+    title: Optional[str] = Field(None, description="Chức danh")
+    department: Optional[str] = Field(None, description="Đơn vị/Phòng ban")
+    reason: Optional[str] = Field(None, description="Lý do khen thưởng")
+    formality: Optional[str] = Field(None, description="Hình thức khen thưởng")
+    amount: Optional[str] = Field(None, description="Số tiền khen thưởng")
+    sign_date: Optional[str] = Field(None, description="Ngày ký")
+    signer: Optional[str] = Field(None, description="Người ký")
--- app/api/v1/endpoints/user/profile/other/felicitation/view.py
+++ app/api/v1/endpoints/user/profile/other/felicitation/view.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from fastapi import APIRouter
 from starlette import status
 
-from app.api.base.schema import PagingResponse
+from app.api.base.schema import ResponseData
 from app.api.base.swagger import swagger_response
 from app.api.v1.endpoints.user.profile.other.felicitation.controller import (
     CtrFelicitation
@@ -18,7 +20,7 @@ router = APIRouter()
     name="[THÔNG TIN KHÁC] - A. KHEN THƯỞNG",
     description="[THÔNG TIN KHÁC] - A. KHEN THƯỞNG",
     responses=swagger_response(
-        response_model=PagingResponse[FelicitionResponse],
+        response_model=ResponseData[List[FelicitionResponse]],
         success_status_code=status.HTTP_200_OK
     )
 )
@@ -26,4 +28,4 @@ async def view_retrieve_current_user(
         # current_user=Depends(get_current_user_from_header())
 ):
     user_info = await CtrFelicitation().ctr_felicitation()
-    return PagingResponse[FelicitionResponse](**user_info)
+    return ResponseData[List[FelicitionResponse]](**user_info)
