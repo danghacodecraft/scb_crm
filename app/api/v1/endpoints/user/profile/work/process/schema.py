f489b7e1f8d79aadfaac8212956b9fad51525576
lebadai <30581741+lebadai@users.noreply.github.com>
2022-04-01 11:31:32 +0700

    [add]: work process

--- app/api/v1/endpoints/user/profile/work/process/schema.py
+++ app/api/v1/endpoints/user/profile/work/process/schema.py
@@ -1,13 +1,15 @@
 from datetime import date
+from typing import Optional
 
 from pydantic import Field
 
 from app.api.base.schema import BaseSchema
+from app.api.v1.schemas.utils import OptionalDropdownResponse
 
 
 # Quá trình công tác
 class WorkProcessResponse(BaseSchema):
-    from_date: date = Field(..., description="Từ ngày")
-    to_date: date = Field(..., description="Đến ngày")
-    company: str = Field(..., description="Công ty")
-    position: str = Field(..., description="Chức vụ")
+    from_date: Optional[date] = Field(..., description="Từ ngày")
+    to_date: Optional[date] = Field(..., description="Đến ngày")
+    company: Optional[str] = Field(..., description="Công ty")
+    position: OptionalDropdownResponse = Field(..., description="Chức vụ")
--- app/api/v1/endpoints/user/profile/work/process/view.py
+++ app/api/v1/endpoints/user/profile/work/process/view.py
@@ -1,7 +1,9 @@
+from typing import List
+
 from fastapi import APIRouter
 from starlette import status
 
-from app.api.base.schema import PagingResponse
+from app.api.base.schema import ResponseData
 from app.api.base.swagger import swagger_response
 from app.api.v1.endpoints.user.profile.work.process.controller import (
     CtrProcess
@@ -18,7 +20,7 @@ router = APIRouter()
     name="[Quá trình công tác] - C. Quá trình công tác",
     description="[Quá trình công tác] - C. Quá trình công tác",
     responses=swagger_response(
-        response_model=PagingResponse[WorkProcessResponse],
+        response_model=ResponseData[List[WorkProcessResponse]],
         success_status_code=status.HTTP_200_OK
     )
 )
@@ -27,4 +29,4 @@ async def view_work_process(
 ):
     user_info = await CtrProcess().ctr_process()
 
-    return PagingResponse[WorkProcessResponse](**user_info)
+    return ResponseData[List[WorkProcessResponse]](**user_info)
