8d26f58faf9c00c4a22b88db0b268890237ce3b1
lebadai <30581741+lebadai@users.noreply.github.com>
2022-04-02 11:58:52 +0700

    [add]: contract

--- app/api/v1/endpoints/user/profile/work/contract/view.py
+++ app/api/v1/endpoints/user/profile/work/contract/view.py
@@ -3,10 +3,10 @@ from starlette import status
 
 from app.api.base.schema import ResponseData
 from app.api.base.swagger import swagger_response
-from app.api.v1.endpoints.user.profile.work.decisive_contract.controller import (
-    CtrDecisiveContract
+from app.api.v1.endpoints.user.profile.work.contract.controller import (
+    CtrContract
 )
-from app.api.v1.endpoints.user.profile.work.decisive_contract.schema import (
+from app.api.v1.endpoints.user.profile.work.contract.schema import (
     ContractInfoResponse
 )
 
@@ -22,8 +22,8 @@ router = APIRouter()
         success_status_code=status.HTTP_200_OK
     )
 )
-async def view_decisive_contract(
+async def view_contract_info(
         employee_id: str = Query(..., description="employee_id")
 ):
-    user_info = await CtrDecisiveContract().ctr_decisive_contract(employee_id=employee_id)
-    return ResponseData[ContractInfoResponse](**user_info)
+    contract_info = await CtrContract().ctr_contract_info(employee_id=employee_id)
+    return ResponseData[ContractInfoResponse](**contract_info)
