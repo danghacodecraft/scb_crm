5b5af7f8d5619026cb64fd73d59410d2fa5b448d
lebadai <30581741+lebadai@users.noreply.github.com>
2022-04-04 14:39:58 +0700

    [add]: foreign

--- app/api/v1/endpoints/user/profile/level/foreign/repository.py
+++ app/api/v1/endpoints/user/profile/level/foreign/repository.py
@@ -9,15 +9,5 @@ async def repos_foreign(
         session: Session
 ) -> ReposReturn:
     data_response = await service_dwh.detail(employee_id=employee_id)
-    # data_response = {
-    #     "language_type": "Anh",
-    #     "level": {
-    #         "id": 1,
-    #         "code": "Code",
-    #         "name": "B"
-    #     },
-    #     "gpa": None,
-    #     "certification_date": None
-    # }
 
     return ReposReturn(data=data_response)
