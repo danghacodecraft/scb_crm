8e7ed477fe02f87ffc22a821b47f20c6c51285df
lebadai <30581741+lebadai@users.noreply.github.com>
2022-04-04 14:43:40 +0700

    [add]: it

--- app/api/v1/endpoints/user/profile/level/it/repository.py
+++ app/api/v1/endpoints/user/profile/level/it/repository.py
@@ -9,11 +9,5 @@ async def repos_it(
         session: Session
 ) -> ReposReturn:
     data_response = await service_dwh.detail(employee_id=employee_id)
-    # data_response = [
-    #     {
-    #         "certification": "Tin học văn phòng A",
-    #         "level": "Kỹ thuật viên",
-    #         "gpa": "7.5"
-    #     }
-    # ]
+
     return ReposReturn(data=data_response)
