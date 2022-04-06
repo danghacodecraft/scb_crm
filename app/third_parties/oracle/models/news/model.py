1c8d49de7fae4a989f0db107d11d277b9034f7f3
aitcq <aitcq@scb.com.vn>
2022-04-06 10:57:58 +0700

    [refactor]: News Model

--- app/third_parties/oracle/models/news/model.py
+++ app/third_parties/oracle/models/news/model.py
@@ -1,5 +1,4 @@
-from cx_Oracle import NCLOB
-from sqlalchemy import VARCHAR, Column, DateTime, ForeignKey, text
+from sqlalchemy import CLOB, VARCHAR, Column, DateTime, ForeignKey, text
 from sqlalchemy.dialects.oracle import NUMBER
 from sqlalchemy.orm import relationship
 
@@ -14,12 +13,12 @@ class News(Base):
     id = Column('news_id', VARCHAR(36), primary_key=True, server_default=text("sys_guid() "),
                 comment='Mã gen tự động tin tức')
     title = Column('title', VARCHAR(1000), nullable=False, comment='Tiêu đề tin tức')
-    avatar_uuid = Column('avatar_url', VARCHAR(36), comment='Tiêu đề tin tức')
-    thumbnail_uuid = Column('thumbnail_url', VARCHAR(36), comment='Banner tin tức')
+    avatar_uuid = Column(VARCHAR(36), comment='Avatar uuid')
+    thumbnail_uuid = Column(VARCHAR(36), comment='Banner tin tức')
     category_id = Column(ForeignKey('crm_news_category.news_category_id'))
     user_id = Column('user_id', VARCHAR(36), comment='Mã user tạo tin')
     user_name = Column('user_name', VARCHAR(255), comment='Tên user tạo tin')
-    content = Column('content', NCLOB, comment='Nội dung tin tức')
+    content = Column('content', CLOB, comment='Nội dung tin tức')
     summary = Column('summary', VARCHAR(2000), comment='Tóm tắt nội dung tin tức')
     created_at = Column('created_at', DateTime, nullable=False, comment='Ngày tạo')
     updated_at = Column('updated_at', DateTime, comment='Ngày cập nhật')
