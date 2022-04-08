343d58531e7b0fa6ae029d00bae2456872f75d71
lebadai <30581741+lebadai@users.noreply.github.com>
2022-04-08 16:07:38 +0700

    [add]: get StageAction

--- app/api/v1/endpoints/config/approval/view.py
+++ app/api/v1/endpoints/config/approval/view.py
@@ -7,7 +7,7 @@ from app.api.base.schema import ResponseData
 from app.api.base.swagger import swagger_response
 from app.api.v1.dependencies.authenticate import get_current_user_from_header
 from app.api.v1.endpoints.config.approval.controller import CtrStageAction
-from app.api.v1.schemas.utils import DropdownStageActionResponse
+from app.api.v1.schemas.utils import DropdownResponse
 
 router = APIRouter()
 
@@ -17,7 +17,7 @@ router = APIRouter()
     name="Stage Action",
     description="Hành động",
     responses=swagger_response(
-        response_model=ResponseData[List[DropdownStageActionResponse]],
+        response_model=ResponseData[List[DropdownResponse]],
         success_status_code=status.HTTP_200_OK
     )
 )
@@ -25,4 +25,4 @@ async def view_province_info(
     stage_action=Depends(get_current_user_from_header())
 ):
     stage_action_info = await CtrStageAction(stage_action).ctr_stage_action()
-    return ResponseData[List[DropdownStageActionResponse]](**stage_action_info)
+    return ResponseData[List[DropdownResponse]](**stage_action_info)
