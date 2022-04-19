3682705872f175d9ad2a13a9187cc92f8adb9d9d
aitcq <aitcq@scb.com.vn>
2022-04-19 13:39:39 +0700

    [add](#78): Enable/Disable Stage

--- app/api/v1/controller.py
+++ app/api/v1/controller.py
@@ -47,3 +47,37 @@ class PermissionController(BaseController):
                 error_status_code=status.HTTP_403_FORBIDDEN
             )
         return ReposReturn(data=None)
+
+    @staticmethod
+    async def ctr_approval_check_permission_stage(
+            auth_response: AuthResponse,
+            menu_code: str,
+            group_role_code: str,
+            permission_code: str,
+            stage_code: Optional[str] = None
+    ) -> ReposReturn:
+        """
+        Kiểm tra xem User có được thao tác ở bước đó không
+        """
+        current_user = auth_response.user_info
+        menu_list = auth_response.menu_list
+        try:
+            filter_code = list(filter(lambda x: x.menu_code == menu_code, menu_list))[0]
+            filter_group_code = \
+                list(filter(lambda x: x.group_role_code == group_role_code, filter_code.group_role_list))[0]
+        except IndexError:
+            return ReposReturn(
+                is_error=True,
+                msg=ERROR_PERMISSION,
+                loc=f"Stage: {stage_code}, User: {current_user.code}",
+                detail=MESSAGE_STATUS[ERROR_PERMISSION],
+                error_status_code=status.HTTP_403_FORBIDDEN,
+                data=False
+            )
+        filter_permission_code = list(filter(
+            lambda x: x.permission_code == permission_code, filter_group_code.permission_list
+        ))
+
+        if not filter_permission_code or not filter_group_code.is_permission:
+            return ReposReturn(data=False)
+        return ReposReturn(data=True)
