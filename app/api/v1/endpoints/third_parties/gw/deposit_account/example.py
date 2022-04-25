0b40d3a3a3fa221162db9a6080e4f39a136378fd
hadh2 <hadh2@scb.com.vn>
2022-04-25 13:43:31 +0700

    [add](#138): [Third-Party][GW][DepositAccount]

---
+++ app/api/v1/endpoints/third_parties/gw/deposit_account/example.py
@@ -0,0 +1,180 @@
+DEPOSIT_CIF_NUMBER_REQUEST = {"cif_number": "0578213"}
+
+DEPOSIT_ACCOUNT_NUMBER = "account_number: 0010108370590001"
+
+DEPOSIT_ACCOUNT_BY_CIF_NUMBER_SUCCESS_EXAMPLE = {
+    "example": {
+        "value": {
+            "data": {
+                "total_balances": "800000000",
+                "total_items": 4,
+                "account_info_list": [
+                    {
+                        "number": "13784840025",
+                        "term": "",
+                        "type": "TD",
+                        "type_name": "Y",
+                        "currency": "VND",
+                        "balance": "200000000",
+                        "balance_available": "200000000.0",
+                        "balance_lock": "",
+                        "open_date": "2020-04-01 00:00:00",
+                        "maturity_date": "2021-07-14 00:00:00",
+                        "saving_serials": "CD0046588",
+                        "class_name": "CHUNG CHI LAI CKY KY HAN 469D",
+                        "class_code": "CCD469",
+                        "interest_rate": "8.9",
+                        "lock_status": "NONE BLOCK",
+                        "branch_info": {
+                            "code": "007",
+                            "name": "007"
+                        },
+                        "payin_account": {
+                            "number": "13784840001"
+                        },
+                        "payout_account": {
+                            "number": "13784840025"
+                        }
+                    },
+                    {
+                        "number": "13784840026",
+                        "term": "",
+                        "type": "TD",
+                        "type_name": "Y",
+                        "currency": "VND",
+                        "balance": "200000000",
+                        "balance_available": "200000000.0",
+                        "balance_lock": "",
+                        "open_date": "2020-04-01 00:00:00",
+                        "maturity_date": "2021-07-14 00:00:00",
+                        "saving_serials": "CD0046590",
+                        "class_name": "CHUNG CHI LAI CKY KY HAN 469D",
+                        "class_code": "CCD469",
+                        "interest_rate": "8.9",
+                        "lock_status": "NONE BLOCK",
+                        "branch_info": {
+                            "code": "007",
+                            "name": "007"
+                        },
+                        "payin_account": {
+                            "number": "13784840001"
+                        },
+                        "payout_account": {
+                            "number": "13784840026"
+                        }
+                    },
+                    {
+                        "number": "13784840027",
+                        "term": "",
+                        "type": "TD",
+                        "type_name": "Y",
+                        "currency": "VND",
+                        "balance": "200000000",
+                        "balance_available": "200000000.0",
+                        "balance_lock": "",
+                        "open_date": "2020-04-01 00:00:00",
+                        "maturity_date": "2021-07-14 00:00:00",
+                        "saving_serials": "CD0046591",
+                        "class_name": "CHUNG CHI LAI CKY KY HAN 469D",
+                        "class_code": "CCD469",
+                        "interest_rate": "8.9",
+                        "lock_status": "NONE BLOCK",
+                        "branch_info": {
+                            "code": "007",
+                            "name": "007"
+                        },
+                        "payin_account": {
+                            "number": "13784840001"
+                        },
+                        "payout_account": {
+                            "number": "13784840027"
+                        }
+                    },
+                    {
+                        "number": "13784840028",
+                        "term": "",
+                        "type": "TD",
+                        "type_name": "Y",
+                        "currency": "VND",
+                        "balance": "200000000",
+                        "balance_available": "200000000.0",
+                        "balance_lock": "",
+                        "open_date": "2020-04-01 00:00:00",
+                        "maturity_date": "2021-07-14 00:00:00",
+                        "saving_serials": "CD0046592",
+                        "class_name": "CHUNG CHI LAI CKY KY HAN 469D",
+                        "class_code": "CCD469",
+                        "interest_rate": "8.9",
+                        "lock_status": "NONE BLOCK",
+                        "branch_info": {
+                            "code": "007",
+                            "name": "007"
+                        },
+                        "payin_account": {
+                            "number": "13784840001"
+                        },
+                        "payout_account": {
+                            "number": "13784840028"
+                        }
+                    }
+                ]
+            }
+        }
+    }
+}
+
+DEPOSIT_ACCOUNT_TD_SUCCESS_EXAMPLE = {
+    "example": {
+        "value": {
+            "data": {
+                "customer_info": {
+                    "fullname_vn": "TRẦN THỊ THANH MAI",
+                    "date_of_birth": "1958-09-14 00:00:00",
+                    "gender": "F",
+                    "email": "",
+                    "mobile_phone": "0903670185",
+                    "type": "I"
+                },
+                "cif_info": {
+                    "cif_number": "0561561",
+                    "issued_date": "2014-02-12 00:00:00"
+                },
+                "account_info": {
+                    "number": "05615610003",
+                    "term": "",
+                    "type": "TD",
+                    "type_name": "Y",
+                    "saving_serials": "FD9235829",
+                    "currency": "VND",
+                    "balance": "300000000",
+                    "balance_available": "300000000.0",
+                    "open_date": "2018-12-21 00:00:00",
+                    "maturity_date": "2019-12-21 00:00:00",
+                    "lock_status": "NONE BLOCK",
+                    "class_name": "TIET KIEM LAI CKY KY HAN 12M TIET KIEM DAC LOC TAI",
+                    "class_code": "AK0E12",
+                    "interest_rate": "8.63",
+                    "branch_info": {
+                        "code": "001",
+                        "name": "CN CONG QUYNH "
+                    },
+                    "payin_account": {
+                        "number": "0010700000020027"
+                    },
+                    "payout_account": {
+                        "number": "05615610003"
+                    },
+                    "staff_info_direct": {
+                        "code": "",
+                        "name": ""
+                    },
+                    "staff_info_indirect": {
+                        "code": "",
+                        "name": ""
+                    }
+                }
+            },
+            "errors": []
+        }
+    }
+}
