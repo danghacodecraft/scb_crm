3f5e244db07bc0168e67da08e2558d358451c824
lebadai <30581741+lebadai@users.noreply.github.com>
2022-05-04 14:14:39 +0700

    [refactor] add optional, none in schema

--- app/api/v1/endpoints/third_parties/gw/schema.py
+++ app/api/v1/endpoints/third_parties/gw/schema.py
@@ -1,13 +1,27 @@
-from pydantic import Field
+from typing import Optional
+
+from pydantic import Field, validator
 
 from app.api.base.schema import BaseSchema
 
 
 class GWBranchDropdownResponse(BaseSchema):
-    code: str = Field(..., description="Mã đơn vị")
-    name: str = Field(..., description="Tên đơn vị không dấu")
+    code: Optional[str] = Field(..., description="Mã đơn vị")
+    name: Optional[str] = Field(..., description="Tên đơn vị không dấu")
+
+    @validator('*', pre=True)
+    def check_blank_str(string):
+        if string == '':
+            return None
+        return string
 
 
 class GWCIFInfoResponse(BaseSchema):
-    cif_number: str = Field(..., description="Số CIF")
-    issued_date: str = Field(..., description="Ngày cấp số CIF")
+    cif_number: Optional[str] = Field(..., description="Số CIF")
+    issued_date: Optional[str] = Field(..., description="Ngày cấp số CIF")
+
+    @validator('*', pre=True)
+    def check_blank_str(string):
+        if string == '':
+            return None
+        return string
