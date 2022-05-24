f510116d1e11c5e627d3c6c71cdc11167d181aa4
hadh2 <hadh2@scb.com.vn>
2022-05-24 09:15:49 +0700

    [refactor](gw): base gw schema

--- app/api/v1/endpoints/third_parties/gw/casa_account/schema.py
+++ app/api/v1/endpoints/third_parties/gw/casa_account/schema.py
@@ -3,7 +3,7 @@ from typing import List, Optional
 
 from pydantic import Field
 
-from app.api.base.schema import BaseGWSchema, BaseSchema
+from app.api.base.schema import BaseGWSchema
 from app.api.v1.endpoints.cif.base_field import CustomField
 from app.api.v1.endpoints.third_parties.gw.casa_account.example import (
     CASA_ACCOUNT_NUMBER
@@ -14,7 +14,7 @@ from app.api.v1.endpoints.third_parties.gw.schema import (
 from app.api.v1.schemas.utils import DropdownResponse
 
 
-class CasaAccountByCIFNumberResponse(BaseSchema):
+class CasaAccountByCIFNumberResponse(BaseGWSchema):
     number: Optional[str] = Field(..., description="Số tài khoản")
     type: Optional[str] = Field(..., description="Loại tài khoản (thanh toán, tiết kiệm…)")
     type_name: Optional[str] = Field(..., description="Tên loại tài khoản")
@@ -37,17 +37,17 @@ class CasaAccountByCIFNumberResponse(BaseSchema):
     branch_info: GWBranchDropdownResponse = Field(...)
 
 
-class GWCasaAccountByCIFNumberResponse(BaseSchema):
+class GWCasaAccountByCIFNumberResponse(BaseGWSchema):
     total_balances: int = Field(..., description="Tổng số dư")
     total_items: int = Field(..., description="Số lượng tài khoản")
     account_info_list: List[CasaAccountByCIFNumberResponse] = Field(..., description="Chi tiết tài khoản")
 
 
-class GWCasaAccountByCIFNumberRequest(BaseSchema):
+class GWCasaAccountByCIFNumberRequest(BaseGWSchema):
     cif_number: str = CustomField().CIFNumberField
 
 
-class GWCustomerInfoResponse(BaseSchema):
+class GWCustomerInfoResponse(BaseGWSchema):
     fullname_vn: Optional[str] = Field(..., description="Họ và tên")
     date_of_birth: Optional[str] = Field(..., description="Ngày sinh")
     gender: Optional[str] = Field(..., description="Giới tính")
@@ -87,17 +87,17 @@ class GWAccountInfoResponse(BaseGWSchema):
     product_package: Optional[str] = Field(..., description="Gói sản phẩm")
 
 
-class GWCasaAccountResponse(BaseSchema):
+class GWCasaAccountResponse(BaseGWSchema):
     customer_info: GWCustomerInfoResponse = Field(..., description="Thông tin người sỡ hữu tài khoản")
     cif_info: GWCIFInfoResponse = Field(..., description="Thông tin CIF")
     account_info: GWAccountInfoResponse = Field(..., description="Thông tin tài khoản")
 
 
-class GWReportPieChartHistoryAccountInfoRequest(BaseSchema):
+class GWReportPieChartHistoryAccountInfoRequest(BaseGWSchema):
     account_number: str = Field(..., description="Số tài khoản", example=CASA_ACCOUNT_NUMBER)
 
 
-class GWReportPieChartHistoryAccountInfoResponse(BaseSchema):
+class GWReportPieChartHistoryAccountInfoResponse(BaseGWSchema):
     transaction_type: str = Field(
         ..., description="""Loại giao dịch. VD: Chuyển tiền đi, Chi tiêu thẻ,
         Chuyển tiền đến, Rút tiền mặt, Nộp tiền mặt, Thanh toán hóa đơn, Khác"""
@@ -108,26 +108,26 @@ class GWReportPieChartHistoryAccountInfoResponse(BaseSchema):
     count_percent: float = Field(0, description="Phần trăm số lượng giao dịch")
 
 
-class GWCasaAccountCheckExistResponse(BaseSchema):
+class GWCasaAccountCheckExistResponse(BaseGWSchema):
     is_existed: bool = Field(..., description="Cờ có tồn tại không")
 
 
-class GWCasaAccountCheckExistRequest(BaseSchema):
+class GWCasaAccountCheckExistRequest(BaseGWSchema):
     account_number: str = Field(..., description="Số tài khoản")
 
 
-class GWReportColumnResponse(BaseSchema):
+class GWReportColumnResponse(BaseGWSchema):
     transaction_type: str = Field(..., description="Loại giao dịch. VD: Rút, Gửi")
     transaction_value: int = Field(..., description="Giá trị giao dịch")
 
 
-class GWReportColumnChart(BaseSchema):
+class GWReportColumnChart(BaseGWSchema):
     transaction_date: Optional[date] = Field(..., description="Ngày giao dịch")
     withdraw: Optional[GWReportColumnResponse] = Field(description="Giao dịch rút tiền")
     send: Optional[GWReportColumnResponse] = Field(description="Giao dịch gửi tiền")
 
 
-class GWReportColumnChartHistoryAccountInfoResponse(BaseSchema):
+class GWReportColumnChartHistoryAccountInfoResponse(BaseGWSchema):
     fullname_vn: str = Field(..., description="Họ và tên")
     type: str = Field(..., description="Loại tài khoản (thanh toán, tiết kiệm…)")
     type_name: str = Field(..., description="Tên loại tài khoản")
@@ -137,19 +137,19 @@ class GWReportColumnChartHistoryAccountInfoResponse(BaseSchema):
     report_casa_account: List[GWReportColumnChart] = Field(..., escription="Báo cáo tài khoản CASA")
 
 
-class GWReportColumnChartHistoryAccountInfoRequest(BaseSchema):
+class GWReportColumnChartHistoryAccountInfoRequest(BaseGWSchema):
     account_number: str = Field(..., description="Số tài khoản")
     from_date: date = Field(date(year=2020, month=4, day=20), description="Từ ngày")
     to_date: date = Field(date(year=2025, month=7, day=20), description="Đến ngày")
 
 
-class GWReportStatementHistoryAccountInfoRequest(BaseSchema):
+class GWReportStatementHistoryAccountInfoRequest(BaseGWSchema):
     account_number: str = Field(..., description="Số tài khoản")
     from_date: date = Field(date(year=2020, month=4, day=20), description="Từ ngày")
     to_date: date = Field(date(year=2025, month=7, day=20), description="Đến ngày")
 
 
-class GWReportStatementHistoryAccountInfoResponse(BaseSchema):
+class GWReportStatementHistoryAccountInfoResponse(BaseGWSchema):
     code: Optional[str] = Field(..., description="Mã giao dịch")
     transaction_date: Optional[date] = Field(..., description="Ngày giao dịch")
     description: Optional[str] = Field(..., description="Chi tiết giao dịch")
@@ -160,7 +160,7 @@ class GWReportStatementHistoryAccountInfoResponse(BaseSchema):
     balance: Optional[int] = Field(..., description="Số dư")
 
 
-class GWReportStatementHistoryTDAccountInfoResponse(BaseSchema):
+class GWReportStatementHistoryTDAccountInfoResponse(BaseGWSchema):
     code: Optional[str] = Field(..., description="Mã giao dịch")
     transaction_date: Optional[date] = Field(..., description="Ngày giao dịch")
     description: Optional[str] = Field(..., description="Chi tiết giao dịch")
