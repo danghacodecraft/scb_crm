94e201c2fcea64bcc1d145251f5531cd91280b8e
lebadai <30581741+lebadai@users.noreply.github.com>
2022-05-26 13:33:31 +0700

    [add](#151)[BOOKING][API] Thông tin bình luận

--- app/api/v1/endpoints/booking/controller.py
+++ app/api/v1/endpoints/booking/controller.py
@@ -1,5 +1,7 @@
 from app.api.base.controller import BaseController
-from app.api.v1.endpoints.booking.repository import repo_add_comment
+from app.api.v1.endpoints.booking.repository import (
+    get_list_comment, repo_add_comment
+)
 from app.api.v1.endpoints.booking.schema import NewsCommentRequest
 from app.third_parties.oracle.models.cif.form.model import Booking
 from app.utils.functions import generate_uuid, now
@@ -45,3 +47,33 @@ class CtrNewsComment(BaseController):
         return self.response(data={
             "comment_id": uuid
         })
+
+    async def ctr_get_comment_by_booking_id(self, booking_id: str):
+        # Validate: check tồn tại booking_id
+        await self.get_model_object_by_id(model_id=booking_id, model=Booking, loc="booking_id")
+        comments = self.call_repos(await get_list_comment(self.oracle_session, booking_id))
+
+        return self.response(data=[dict(
+            id=cmt_item.id,
+            booking_id=cmt_item.booking_id,
+            username=cmt_item.username,
+            name=cmt_item.name,
+            code=cmt_item.code,
+            email=cmt_item.email,
+            hrm_department_id=cmt_item.hrm_department_id,
+            hrm_department_code=cmt_item.hrm_department_code,
+            hrm_department_name=cmt_item.hrm_department_name,
+            hrm_branch_id=cmt_item.hrm_branch_id,
+            hrm_branch_code=cmt_item.hrm_branch_code,
+            hrm_branch_name=cmt_item.hrm_branch_name,
+            hrm_title_id=cmt_item.hrm_title_id,
+            hrm_title_code=cmt_item.hrm_title_code,
+            hrm_title_name=cmt_item.hrm_title_name,
+            hrm_position_id=cmt_item.hrm_position_id,
+            hrm_position_code=cmt_item.hrm_position_code,
+            hrm_position_name=cmt_item.hrm_position_name,
+            content=cmt_item.content,
+            created_at=cmt_item.created_at,
+            updated_at=cmt_item.updated_at,
+            file_uuid=cmt_item.file_uuid
+        ) for cmt_item in comments])
