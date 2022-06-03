0950b6f67f262e2c979f9e102b44958d2b066a8c
hadh2 <hadh2@scb.com.vn>
2022-06-03 09:15:53 +0700

    [add](#156): cif working info

--- app/api/v1/endpoints/cif/schema.py
+++ app/api/v1/endpoints/cif/schema.py
@@ -98,6 +98,12 @@ class CifCustomerInformationResponse(BaseSchema):
     employees: List[EmployeeResponse] = Field(..., description="Danh sách nhân viên")
 
 
+class CifCustomerWorkingInformationResponse(BaseSchema):
+    booking_id: str = Field(..., description="Booking ID")
+    business_type: DropdownResponse = Field(..., description="Loại nghiệp vụ")
+    created_at: datetime = Field(..., description="Ngày tạo")
+
+
 class SOACIFInformation(BaseSchema):
     cif_number: str = CustomField().CIFNumberField
     issued_date: str = Field(None, description="Ngày cấp số CIF")
