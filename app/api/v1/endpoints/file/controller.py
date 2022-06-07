918c4e88440b92f74e98087a7910bb67d70a4532
cuonglv4 <cuonglv4@scb.com.vn>
2022-06-07 13:36:15 +0700

    [refactor]: fix upload file ekyc

--- app/api/v1/endpoints/file/controller.py
+++ app/api/v1/endpoints/file/controller.py
@@ -74,6 +74,7 @@ class CtrFile(BaseController):
         ))
 
         service = ServiceEKYC()
-        info_file = await service.upload_file_ekyc(info=info, booking_id=booking_id)
-
+        is_success, info_file = await service.upload_file_ekyc(info=info, booking_id=booking_id)
+        if not is_success:
+            return self.response_exception(msg="UPLOAD_FILE_EKYC", loc="UPLOAD_EKYC_FILE")
         return self.response(data=info_file)
