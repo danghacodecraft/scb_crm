bda80f9bb1472b0c3afa1702008fecbbfbd1c06e
lebadai <30581741+lebadai@users.noreply.github.com>
2022-06-15 16:23:05 +0700

    [add](#164) [CASA][API] GET - Đồng sở hữu tài khoản

--- app/api/v1/endpoints/casa/open_casa/co_owner/view.py
+++ app/api/v1/endpoints/casa/open_casa/co_owner/view.py
@@ -6,9 +6,8 @@ from app.api.base.swagger import swagger_response
 from app.api.v1.dependencies.authenticate import get_current_user_from_header
 from app.api.v1.endpoints.casa.open_casa.co_owner.controller import CtrCoOwner
 from app.api.v1.endpoints.casa.open_casa.co_owner.schema import (
-    AccountHolderRequest
+    AccountHolderRequest, CoOwnerRequest, GetCoOwnerResponse
 )
-from app.api.v1.schemas.utils import SaveSuccessResponse
 
 router = APIRouter()
 
@@ -18,7 +17,7 @@ router = APIRouter()
     name="Đồng sở hữu tài khoản",
     description="Đồng sở hữu tài khoản",
     responses=swagger_response(
-        response_model=ResponseData[SaveSuccessResponse],
+        response_model=ResponseData[CoOwnerRequest],
         success_status_code=status.HTTP_200_OK
     )
 )
@@ -29,4 +28,22 @@ async def view_create_co_owner(
         current_user=Depends(get_current_user_from_header())
 ):
     co_owner_data = await CtrCoOwner(current_user).ctr_save_co_owner(account_id, co_owner, booking_id=BOOKING_ID)
-    return ResponseData[SaveSuccessResponse](**co_owner_data)
+    return ResponseData[CoOwnerRequest](**co_owner_data)
+
+
+@router.get(
+    path="/{account_id}/co-owner",
+    name="Đồng sở hữu tài khoản",
+    description="Đồng sở hữu tài khoản",
+    responses=swagger_response(
+        response_model=ResponseData[GetCoOwnerResponse],
+        success_status_code=status.HTTP_200_OK
+    ),
+)
+async def view_retrieve_co_owner(
+        account_id: str = Path(..., description='Mã tài khoản thanh toán'),
+        BOOKING_ID: str = Header(..., description="Mã phiên giao dịch"),  # noqa
+        current_user=Depends(get_current_user_from_header())
+):
+    co_owner_data = await CtrCoOwner(current_user).ctr_co_owner(account_id, booking_id=BOOKING_ID)
+    return ResponseData[GetCoOwnerResponse](**co_owner_data)
