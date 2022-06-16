0d0ba6524a36829d1fb31f667b4f5cb2b7b26391
cuonglv4 <cuonglv4@scb.com.vn>
2022-06-16 15:08:44 +0700

    [add](#167): detail user core

---
+++ app/api/v1/endpoints/third_parties/gw/user/view.py
@@ -0,0 +1,32 @@
+from typing import List
+
+from fastapi import APIRouter, Depends, Path
+from starlette import status
+
+from app.api.base.schema import ResponseData
+from app.api.base.swagger import swagger_response
+from app.api.v1.dependencies.authenticate import get_current_user_from_header
+from app.api.v1.endpoints.third_parties.gw.user.controller import CtrGWUser
+from app.api.v1.endpoints.third_parties.gw.user.schema import (
+    GWDetailUserInfoResponse
+)
+
+router = APIRouter()
+
+
+@router.post(
+    path="/{user_id}/",
+    name="[GW] Amount Block",
+    description="Phong tỏa tài khoản",
+    responses=swagger_response(
+        response_model=ResponseData[List[GWDetailUserInfoResponse]],
+        success_status_code=status.HTTP_200_OK
+    )
+)
+async def view_gw_amount_block(
+        user_id: str = Path(..., description="Số tài khoản"),
+        current_user=Depends(get_current_user_from_header())
+):
+    gw_detail_user = await CtrGWUser(current_user).ctr_gw_detail_user(user_id=user_id)
+
+    return ResponseData[List[GWDetailUserInfoResponse]](**gw_detail_user)
--- app/third_parties/services/gw.py
+++ app/third_parties/services/gw.py
@@ -43,7 +43,8 @@ from app.utils.constant.gw import (
     GW_ENDPOINT_URL_RETRIEVE_TOPIC_INFO_FROM_CODE,
     GW_ENDPOINT_URL_RETRIEVE_WORKING_PROCESS_INFO_FROM_CODE,
     GW_ENDPOINT_URL_SELECT_CATEGORY,
-    GW_ENDPOINT_URL_SELECT_EMPLOYEE_INFO_FROM_CODE, GW_FUNCTION_OPEN_CASA,
+    GW_ENDPOINT_URL_SELECT_EMPLOYEE_INFO_FROM_CODE,
+    GW_ENDPOINT_URL_SELECT_USER_INFO, GW_FUNCTION_OPEN_CASA,
     GW_HISTORY_ACCOUNT_NUM, GW_HISTORY_CHANGE_FIELD_ACCOUNT,
     GW_RETRIEVE_CASA_ACCOUNT_DETAIL
 )
@@ -1695,6 +1696,45 @@ class ServiceGW:
             current_user=current_user, function_name="payInCash_in", data_input=data_input
         )
         api_url = f"{self.url}{GW_ENDPOINT_URL_PAY_IN_CASH}"
+
+        return_errors = dict(
+            loc="SERVICE GW",
+            msg="",
+            detail=""
+        )
+        return_data = dict(
+            status=None,
+            data=None,
+            errors=return_errors
+        )
+
+        try:
+            async with self.session.post(url=api_url, json=request_data) as response:
+                logger.log("SERVICE", f"[GW][Payment] {response.status} {api_url}")
+                if response.status != status.HTTP_200_OK:
+                    if response.status < status.HTTP_500_INTERNAL_SERVER_ERROR:
+                        return_error = await response.json()
+                        return_data.update(
+                            status=response.status,
+                            errors=return_error['errors']
+                        )
+                    return False, return_data
+                else:
+                    return_data = await response.json()
+                    return True, return_data
+        except aiohttp.ClientConnectorError as ex:
+            logger.error(str(ex))
+            return False, return_data
+
+    ####################################################################################################################
+    # start --- user
+    ####################################################################################################################
+    async def gw_detail_user(self, current_user: UserInfoResponse, data_input):
+
+        request_data = self.gw_create_request_body(
+            current_user=current_user, function_name="selectUserInfoByUserID_in", data_input=data_input
+        )
+        api_url = f"{self.url}{GW_ENDPOINT_URL_SELECT_USER_INFO}"
 
         return_errors = dict(
             loc="SERVICE GW",
