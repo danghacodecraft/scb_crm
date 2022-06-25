bd572a7f57a46a4acdb58c43fd3b866f35c897f1
aitcq <aitcq@scb.com.vn>
2022-06-25 22:55:35 +0700

    [refactor](#56): code

--- app/api/v1/endpoints/cif/other_information/repository.py
+++ app/api/v1/endpoints/cif/other_information/repository.py
@@ -19,7 +19,7 @@ from app.third_parties.oracle.models.master_data.others import StaffType
 from app.utils.constant.cif import (
     BUSINESS_FORM_TTK, STAFF_TYPE_BUSINESS_CODE, STAFF_TYPE_REFER_INDIRECT_CODE
 )
-from app.utils.error_messages import ERROR_CIF_ID_NOT_EXIST, ERROR_NO_DATA
+from app.utils.error_messages import ERROR_CIF_ID_NOT_EXIST
 
 
 async def repos_other_info(cif_id: str, session: Session) -> ReposReturn:
@@ -41,33 +41,7 @@ async def repos_other_info(cif_id: str, session: Session) -> ReposReturn:
     if not customer_employee:
         return ReposReturn(is_error=True, msg=ERROR_CIF_ID_NOT_EXIST, loc="cif_id")
 
-    sale_staff = None
-    indirect_sale_staff = None
-
-    for _, staff_type, employee in customer_employee:
-        if staff_type and employee:
-            if staff_type.code == STAFF_TYPE_BUSINESS_CODE:
-                sale_staff = {
-                    "id": employee.employee_id,
-                    "fullname_vn": None
-                }
-            if staff_type.code == STAFF_TYPE_REFER_INDIRECT_CODE:
-                indirect_sale_staff = {
-                    "id": employee.employee_id,
-                    "fullname_vn": None
-                }
-    legal_agreement_flag = customer_employee[0][0].legal_agreement_flag
-    advertising_marketing_flag = customer_employee[0][0].advertising_marketing_flag
-    if legal_agreement_flag is None or advertising_marketing_flag is None:
-        return ReposReturn(is_error=True, msg=ERROR_NO_DATA)
-
-    return ReposReturn(data={
-        # lấy ra data ở vị trí 0 trong query sau đó lấy ra customer ở vị trí 0
-        "legal_agreement_flag": legal_agreement_flag,
-        "advertising_marketing_flag": advertising_marketing_flag,
-        "sale_staff": sale_staff,
-        "indirect_sale_staff": indirect_sale_staff,
-    })
+    return ReposReturn(data=customer_employee)
 
 
 @auto_commit
