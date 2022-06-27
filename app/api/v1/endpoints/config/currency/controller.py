17c15b79ee1ccfd6b59e7bdde595a393c455dbc3
cuonglv4 <cuonglv4@scb.com.vn>
2022-06-27 15:02:39 +0700

    [refactor](#55): config currency

--- app/api/v1/endpoints/config/currency/controller.py
+++ app/api/v1/endpoints/config/currency/controller.py
@@ -1,14 +1,21 @@
 from app.api.base.controller import BaseController
-from app.api.v1.endpoints.repository import repos_get_data_model_config
-from app.third_parties.oracle.models.master_data.others import Currency
+from app.api.v1.endpoints.repository import repos_get_data_currency_config
 
 
 class CtrConfigCurrency(BaseController):
     async def ctr_currency_info(self):
         currency_info = self.call_repos(
-            await repos_get_data_model_config(
-                session=self.oracle_session,
-                model=Currency
+            await repos_get_data_currency_config(
+                session=self.oracle_session
             )
         )
-        return self.response(currency_info)
+        response_data = [{
+            'currency_id': item.Currency.id,
+            'currency_code': item.Currency.code,
+            'currency_name': item.Currency.name,
+            'country_id': item.AddressCountry.id,
+            'country_code': item.AddressCountry.code,
+            'country_name': item.AddressCountry.name,
+        } for item in currency_info]
+
+        return self.response(response_data)
