a486dd4bacbd8a8d929e4dbfa96d8d222674da7f
Phạm Thành Đạt <datpt@minerva.vn>
2022-06-28 11:58:48 +0700

    [update] login repos

--- app/api/v1/endpoints/user/repository.py
+++ app/api/v1/endpoints/user/repository.py
@@ -7,7 +7,7 @@ from starlette import status
 
 from app.api.base.repository import ReposReturn
 from app.api.v1.endpoints.user.schema import UserInfoResponse
-from app.settings.event import service_idm, service_gw
+from app.settings.event import service_gw, service_idm
 from app.third_parties.services.idm import ServiceIDM
 from app.utils.error_messages import (
     ERROR_CALL_SERVICE_IDM, ERROR_INVALID_TOKEN, USER_ID_NOT_EXIST
@@ -40,44 +40,6 @@ async def repos_login(username: str, password: str) -> ReposReturn:
             detail=str(data_idm)
         )
 
-    data_idm["user_info"]["avatar_url"] = ServiceIDM().replace_with_cdn(data_idm["user_info"]["avatar_url"])
-    data_idm['user_info']['token'] = base64.b64encode(
-        zlib.compress(orjson.dumps(data_idm))
-    ).decode('utf-8')
-
-    lst_data = []
-    list(map(lambda x: lst_data.extend(x['group_role_list']), data_idm['menu_list']))
-    filter_permission_code = list(filter(
-        lambda x: x.get('is_permission') is True,
-        list(filter(
-            lambda item: len(
-                list(filter(
-                    lambda permission: permission.get('permission_code') == "ACCESS", item['permission_list']))
-            ) > 0, lst_data))
-
-    ))
-    # try:
-    #     # Check has permission in IDM
-    #     filter_code = list(filter(lambda x: x.get('menu_code') == "HOME", data_idm['menu_list']))[0]
-    #     filter_group_code = list(filter(lambda x: x.get('group_role_code') == "ACCESS", filter_code['group_role_list']))[0]
-    # except IndexError:
-    #     return ReposReturn(
-    #         is_error=True,
-    #         msg="Permission Denied",
-    #         detail="Permission Denied",
-    #         error_status_code=status.HTTP_403_FORBIDDEN
-    #     )
-    # filter_permission_code = list(filter(lambda x: x.get('permission_code') == "ACCESS", filter_group_code['permission_list']))
-    #
-    # if not filter_permission_code or not filter_group_code['is_permission']:
-    if not filter_permission_code:
-        return ReposReturn(
-            is_error=True,
-            msg="Permission Denied",
-            detail="Permission Denied",
-            error_status_code=status.HTTP_403_FORBIDDEN
-        )
-
     is_success, data_gw = await service_gw.gw_detail_user(
         current_user=UserInfoResponse(**data_idm["user_info"]),
         data_input={
@@ -94,9 +56,30 @@ async def repos_login(username: str, password: str) -> ReposReturn:
         data_idm['hrm_branch_name'] = gw_data_output['branch_info']['branch_code']
         data_idm['hrm_branch_name'] = gw_data_output['branch_info']['branch_name']
 
-        data_idm['user_info']['token'] = base64.b64encode(
-            zlib.compress(orjson.dumps(data_idm))
-        ).decode('utf-8')
+    data_idm["user_info"]["avatar_url"] = ServiceIDM().replace_with_cdn(data_idm["user_info"]["avatar_url"])
+    data_idm['user_info']['token'] = base64.b64encode(
+        zlib.compress(orjson.dumps(data_idm))
+    ).decode('utf-8')
+
+    lst_data = []
+    list(map(lambda x: lst_data.extend(x['group_role_list']), data_idm['menu_list']))
+    filter_permission_code = list(filter(
+        lambda x: x.get('is_permission') is True,
+        list(filter(
+            lambda item: len(
+                list(filter(
+                    lambda permission: permission.get('permission_code') == "ACCESS", item['permission_list']))
+            ) > 0, lst_data))
+
+    ))
+
+    if not filter_permission_code:
+        return ReposReturn(
+            is_error=True,
+            msg="Permission Denied",
+            detail="Permission Denied",
+            error_status_code=status.HTTP_403_FORBIDDEN
+        )
 
     return ReposReturn(data=data_idm)
 
