4b8efb2ea9e4e5d33829ba540e6b10c365ade56d
cuonglv4 <cuonglv4@scb.com.vn>
2022-07-05 11:38:52 +0700

    [add](#55): config td_account

--- app/api/v1/endpoints/config/deposit/controller.py
+++ app/api/v1/endpoints/config/deposit/controller.py
@@ -1,4 +1,9 @@
 from app.api.base.controller import BaseController
+from app.api.v1.endpoints.config.deposit.repository import (
+    repos_get_acc_class, repos_get_acc_type, repos_get_interest_type_by_id
+)
+from app.api.v1.endpoints.repository import repos_get_data_model_config
+from app.third_parties.oracle.models.cif.e_banking.model import TdInterestType
 
 
 class CtrConfigDeposit(BaseController):
@@ -16,3 +21,44 @@ class CtrConfigDeposit(BaseController):
             }
         ]
         return self.response(data=rollover_type)
+
+    async def ctr_get_interest_type(self):
+        interest_type = self.call_repos(
+            await repos_get_data_model_config(
+                session=self.oracle_session,
+                model=TdInterestType
+            )
+        )
+
+        return self.response(data=interest_type)
+
+    async def ctr_get_acc_type(self):
+        current = self.current_user
+
+        acc_type = self.call_repos(
+            await repos_get_acc_type(
+                current_user=current.user_info
+            )
+        )
+        return self.response(data=acc_type)
+
+    async def ctr_get_acc_class(self, acc_class_request):
+        current = self.current_user
+
+        interest = self.call_repos(
+            await repos_get_interest_type_by_id(
+                interest_type_id=acc_class_request.interest_type_id,
+                session=self.oracle_session
+            )
+        )
+
+        acc_class = self.call_repos(
+            await repos_get_acc_class(
+                current_user=current.user_info,
+                interest=interest,
+                acc_type=acc_class_request.acc_type,
+                currency_id=acc_class_request.currency_id
+            )
+        )
+
+        return self.response(data=acc_class)
--- app/api/v1/endpoints/config/deposit/view.py
+++ app/api/v1/endpoints/config/deposit/view.py
@@ -1,12 +1,15 @@
 from typing import List
 
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Body, Depends
 from starlette import status
 
 from app.api.base.schema import ResponseData
 from app.api.base.swagger import swagger_response
 from app.api.v1.dependencies.authenticate import get_current_user_from_header
 from app.api.v1.endpoints.config.deposit.controller import CtrConfigDeposit
+from app.api.v1.endpoints.config.deposit.schema import (
+    AccClassRequest, AccClassResponse
+)
 from app.api.v1.schemas.utils import DropdownResponse
 
 router = APIRouter()
@@ -24,6 +27,59 @@ router = APIRouter()
 async def view_business_type(
         current_user=Depends(get_current_user_from_header())  # noqa
 ):
-    rollover_type = await CtrConfigDeposit().ctr_get_rollover_type()
+    rollover_type = await CtrConfigDeposit(current_user).ctr_get_rollover_type()
 
     return ResponseData[List[DropdownResponse]](**rollover_type)
+
+
+@router.get(
+    path="/interest-type/",
+    name="Hình thức lãi",
+    description="Hình thức lãi",
+    responses=swagger_response(
+        response_model=ResponseData[List[DropdownResponse]],
+        success_status_code=status.HTTP_200_OK
+    )
+)
+async def view_interest_type(
+        current_user=Depends(get_current_user_from_header())  # noqa
+):
+    interest_type = await CtrConfigDeposit(current_user).ctr_get_interest_type()
+
+    return ResponseData[List[DropdownResponse]](**interest_type)
+
+
+@router.get(
+    path="/acc_type/",
+    name="Sản phẩm",
+    description="Sản phẩm",
+    responses=swagger_response(
+        response_model=ResponseData[List[DropdownResponse]],
+        success_status_code=status.HTTP_200_OK
+    )
+)
+async def view_acc_type(
+        current_user=Depends(get_current_user_from_header())  # noqa
+):
+    acc_type = await CtrConfigDeposit(current_user).ctr_get_acc_type()
+
+    return ResponseData(**acc_type)
+
+
+@router.get(
+    path="/acc_class/",
+    name="Loại nhóm sản phẩm (gói) tài khoản",
+    description="Loại nhóm sản phẩm (gói) tài khoản",
+    responses=swagger_response(
+        response_model=ResponseData[List[AccClassResponse]],
+        success_status_code=status.HTTP_200_OK
+    )
+)
+async def view_acc_class(
+        acc_class_request: AccClassRequest = Body(...),
+        current_user=Depends(get_current_user_from_header())  # noqa
+):
+    acc_class = await CtrConfigDeposit(current_user).ctr_get_acc_class(
+        acc_class_request=acc_class_request
+    )
+    return ResponseData[List[AccClassResponse]](**acc_class)
