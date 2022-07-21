6c74d0159ce8513cda6b248f88d9c17580bb4ba1
lebadai <30581741+lebadai@users.noreply.github.com>
2022-07-21 09:25:12 +0700

    [add] [GW][Ebank] OpenInternetBanking

--- app/third_parties/services/gw.py
+++ app/third_parties/services/gw.py
@@ -991,34 +991,42 @@ class ServiceGW:
 
         api_url = f"{self.url}{GW_ENDPOINT_URL_OPEN_INTERNET_BANKING}"
 
-        return_errors = dict(
-            loc="SERVICE GW",
-            msg="",
-            detail=""
+        response_data = await self.call_api(
+            request_data=request_data,
+            api_url=api_url,
+            output_key='openIB_out',
+            service_name='openIB'
         )
-        return_data = dict(
-            status=None,
-            data=None,
-            errors=return_errors
-        )
+        return response_data
 
-        try:
-            async with self.session.post(url=api_url, json=request_data) as response:
-                logger.log("SERVICE", f"[GW] {response.status} {api_url}")
-                if response.status != status.HTTP_200_OK:
-                    if response.status < status.HTTP_500_INTERNAL_SERVER_ERROR:
-                        return_error = await response.json()
-                        return_data.update(
-                            status=response.status,
-                            errors=return_error['errors']
-                        )
-                    return False, return_data
-                else:
-                    return_data = await response.json()
-                    return True, return_data
-        except aiohttp.ClientConnectorError as ex:
-            logger.error(str(ex))
-            return False, return_data
+        # return_errors = dict(
+        #     loc="SERVICE GW",
+        #     msg="",
+        #     detail=""
+        # )
+        # return_data = dict(
+        #     status=None,
+        #     data=None,
+        #     errors=return_errors
+        # )
+        #
+        # try:
+        #     async with self.session.post(url=api_url, json=request_data) as response:
+        #         logger.log("SERVICE", f"[GW] {response.status} {api_url}")
+        #         if response.status != status.HTTP_200_OK:
+        #             if response.status < status.HTTP_500_INTERNAL_SERVER_ERROR:
+        #                 return_error = await response.json()
+        #                 return_data.update(
+        #                     status=response.status,
+        #                     errors=return_error['errors']
+        #                 )
+        #             return False, return_data
+        #         else:
+        #             return_data = await response.json()
+        #             return True, return_data
+        # except aiohttp.ClientConnectorError as ex:
+        #     logger.error(str(ex))
+        #     return False, return_data
     ####################################################################################################################
     # END --- OPEN INTERNET BANKING
     ####################################################################################################################
