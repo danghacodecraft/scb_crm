322f73064c6ceb5131077744ac916cd16455054d
lebadai <30581741+lebadai@users.noreply.github.com>
2022-07-29 16:02:01 +0700

    [refactor][Casa][CoOwner] get cif_num in signature_list

--- app/api/v1/endpoints/casa/open_casa/co_owner/view.py
+++ app/api/v1/endpoints/casa/open_casa/co_owner/view.py
@@ -48,4 +48,3 @@ async def view_retrieve_co_owner(
 ):
     co_owner_data = await CtrCoOwner(current_user).ctr_co_owner(account_id, booking_id=BOOKING_ID)
     return ResponseData[GetCoOwnerResponse](**co_owner_data)
-    # return ResponseData(**co_owner_data)
