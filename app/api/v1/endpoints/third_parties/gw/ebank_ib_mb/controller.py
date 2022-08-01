deab607adb6c3c96570a1a8f67cb2a26db28734f
datnt <datnt@minerva.vn>
2022-08-01 09:35:23 +0700

    [add](#224): GW_retrieveMBInfoByCif.

--- app/api/v1/endpoints/third_parties/gw/ebank_ib_mb/controller.py
+++ app/api/v1/endpoints/third_parties/gw/ebank_ib_mb/controller.py
@@ -1,6 +1,7 @@
 from app.api.base.controller import BaseController
 from app.api.v1.endpoints.third_parties.gw.ebank_ib_mb.repository import (
-    repos_gw_get_check_username_ib_mb_exist, repos_gw_retrieve_ib_info_by_cif
+    repos_gw_get_check_username_ib_mb_exist, repos_gw_retrieve_ib_info_by_cif,
+    repos_gw_retrieve_mb_info_by_cif
 )
 
 
@@ -17,6 +18,14 @@ class CtrGWEbankIbMb(BaseController):
     async def ctr_gw_retrieve_ib_info_by_cif(self, cif_num):
         current_user = self.current_user
         ebank_info = self.call_repos(await repos_gw_retrieve_ib_info_by_cif(
+            current_user=current_user,
+            cif_num=cif_num))
+
+        return self.response(data=ebank_info)
+
+    async def ctr_gw_retrieve_mb_info_by_cif(self, cif_num):
+        current_user = self.current_user
+        ebank_info = self.call_repos(await repos_gw_retrieve_mb_info_by_cif(
             current_user=current_user,
             cif_num=cif_num))
 
--- app/api/v1/endpoints/third_parties/gw/ebank_ib_mb/view.py
+++ app/api/v1/endpoints/third_parties/gw/ebank_ib_mb/view.py
@@ -9,10 +9,12 @@ from app.api.v1.endpoints.third_parties.gw.ebank_ib_mb.controller import (
 )
 from app.api.v1.endpoints.third_parties.gw.ebank_ib_mb.schema import (
     CheckUsernameIBMBExistRequest, CheckUsernameIBMBExistResponse,
-    RetrieveIBInfoByCifRequest, RetrieveIBInfoByCifResponse
+    RetrieveIBInfoByCifRequest, RetrieveIBInfoByCifResponse,
+    RetrieveMBInfoByCifRequest, RetrieveMBInfoByCifResponse
 )
 from app.utils.constant.gw import (
-    GW_FUNC_CHECK_USERNAME_IB_MB_EXIST_OUT, GW_FUNC_RETRIEVE_IB_INFO_BY_CIF_OUT
+    GW_FUNC_CHECK_USERNAME_IB_MB_EXIST_OUT,
+    GW_FUNC_RETRIEVE_IB_INFO_BY_CIF_OUT, GW_FUNC_RETRIEVE_MB_INFO_BY_CIF_OUT
 )
 
 router = APIRouter()
@@ -65,3 +67,27 @@ async def view_gw_retrieve_ib_info_by_cif(
         'data').get(GW_FUNC_RETRIEVE_IB_INFO_BY_CIF_OUT).get('data_output')
 
     return ResponseData[RetrieveIBInfoByCifResponse](**gw_get_retrieve_ib_info_by_cif)
+
+
+@router.post(
+    path="/retrieve-mb-info-by-cif/",
+    name="[GW] Lấy chi tiết thông tin dịch vụ MB của khách hàng",
+    description="[GW] Lấy chi tiết thông tin dịch vụ MB của khách hàng",
+    responses=swagger_response(
+        response_model=ResponseData[RetrieveMBInfoByCifResponse],
+        success_status_code=status.HTTP_200_OK
+    )
+)
+async def view_gw_retrieve_mb_info_by_cif(
+        request: RetrieveMBInfoByCifRequest = Body(..., ),
+        current_user=Depends(get_current_user_from_header())
+):
+    gw_get_retrieve_mb_info_by_cif = await CtrGWEbankIbMb(
+        current_user).ctr_gw_retrieve_mb_info_by_cif(
+        cif_num=request.cif_info.cif_num
+    )
+
+    gw_get_retrieve_mb_info_by_cif['data'] = gw_get_retrieve_mb_info_by_cif.get(
+        'data').get(GW_FUNC_RETRIEVE_MB_INFO_BY_CIF_OUT).get('data_output')
+
+    return ResponseData[RetrieveMBInfoByCifResponse](**gw_get_retrieve_mb_info_by_cif)
