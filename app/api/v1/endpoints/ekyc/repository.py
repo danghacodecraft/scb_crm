740e6161463a4469b1e45d74209229a1e9d572f3
aitcq <aitcq@scb.com.vn>
2022-08-03 10:04:35 +0700

    [add]: [EKYC] customer

--- app/api/v1/endpoints/ekyc/repository.py
+++ app/api/v1/endpoints/ekyc/repository.py
@@ -5,9 +5,13 @@ from app.api.base.repository import ReposReturn, auto_commit
 from app.third_parties.oracle.models.eKYC.model import EKYCCustomer, EKYCCustomerStep
 
 
-@auto_commit
-async def repos_create_ekyc_customer(customer: dict, session: Session):
-    session.add(EKYCCustomer(**customer))
+# @auto_commit
+async def repos_create_ekyc_customer(customer: dict, steps, session: Session):
+    insert_list = [EKYCCustomer(**customer)]
+    if steps:
+        for step in steps:
+            insert_list.append(EKYCCustomerStep(**step))
+    session.add_all(insert_list)
     session.flush()
     return ReposReturn(data=None)
 
--- app/api/v1/endpoints/ekyc/schema.py
+++ app/api/v1/endpoints/ekyc/schema.py
@@ -1,5 +1,5 @@
-from datetime import date
-from typing import Optional
+from datetime import date, datetime
+from typing import Optional, List
 
 from pydantic import Field, validator
 
@@ -9,53 +9,68 @@ from app.utils.constant.ekyc import EKYC_DATE_FORMAT
 from app.utils.functions import date_string_to_other_date_string_format
 
 
+class InfoStep(BaseSchema):
+    step: str = Field(...)
+    step_status: str = Field(...)
+    update_at: datetime = Field(...)
+    reason: str = Field(None)
+    start_date: date = Field(None)
+    end_date: date = Field(None)
+
+
+class EKYCStep(BaseSchema):
+    transaction_id: str = Field(...)
+    info_step: List[InfoStep] = Field(...)
+
+
 class CreateEKYCCustomerRequest(BaseSchema):
-    date_of_issue: date = Field(...)
-    date_of_birth: date = Field(...)
-    transaction_id: Optional[str] = Field(...)
-    document_id: str = Field(...)
-    document_type: int = Field(...)
-    date_of_expiry: date = Field(None)
-    place_of_issue: str = Field(None)
-    qr_code_data: str = Field(...)
     customer_id: Optional[str] = Field(..., description='Mã khách hàng (ekyc)')
-    full_name: str = Field(...)
-    gender: str = Field(...)
-    place_of_residence: str = Field(...)
-    place_of_origin: str = Field(...)
-    nationality: str = Field(...)
-    address_1: str = Field(...)
-    address_2: str = Field(...)
-    address_3: str = Field(...)
-    address_4: str = Field(...)
-    permanent_address: dict = Field(...)
-    phone_number: str = Field(...)
-    ocr_data: dict = Field(...)
-    ocr_data_errors: dict = Field(...)
-    faces_matching_percent: Optional[float] = Field(...)
-    extra_info: Optional[dict] = Field(...)
-    receive_ads: bool = Field(...)
-    longitude: Optional[float] = Field(...)
-    latitude: Optional[float] = Field(...)
-    cif: str = Field(...)
-    account_number: str = Field(...)
-    job_title: str = Field(...)
-    organization: str = Field(...)
-    organization_address: str = Field(...)
-    organization_phone_number: str = Field(...)
-    position: str = Field(...)
-    salary_range: str = Field(...)
-    tax_number: str = Field(...)
-    open_biometric: bool = Field(...)
-    avatar_image_url: str = Field(...)
-    avatar_image_uri: str = Field(...)
-    attachment_info: Optional[list] = Field(...)
-    finger_ids: Optional[list] = Field(...)
-    face_ids: Optional[list] = Field(...)
-    ekyc_level: str = Field(...)
-    ekyc_step: Optional[list] = Field(...)
-    kss_status: str = Field(...)
-    status: str = Field(...)
+    transaction_id: Optional[str] = Field(None)
+    date_of_issue: Optional[date] = Field(None)
+    date_of_birth: Optional[date] = Field(None)
+    document_id: Optional[str] = Field(None)
+    document_type: Optional[int] = Field(None)
+    date_of_expiry: Optional[date] = Field(None)
+    place_of_issue: Optional[str] = Field(None)
+    qr_code_data: Optional[str] = Field(None)
+    full_name: Optional[str] = Field(None)
+    gender: Optional[str] = Field(None)
+    place_of_residence: Optional[str] = Field(None)
+    place_of_origin: Optional[str] = Field(None)
+    nationality: Optional[str] = Field(None)
+    address_1: Optional[str] = Field(None)
+    address_2: Optional[str] = Field(None)
+    address_3: Optional[str] = Field(None)
+    address_4: Optional[str] = Field(None)
+    permanent_address: Optional[dict] = Field(None)
+    phone_number: Optional[str] = Field(None)
+    ocr_data: Optional[dict] = Field(None)
+    ocr_data_errors: Optional[dict] = Field(None)
+    faces_matching_percent: Optional[float] = Field(None)
+    extra_info: Optional[dict] = Field(None)
+    receive_ads: Optional[bool] = Field(None)
+    longitude: Optional[float] = Field(None)
+    latitude: Optional[float] = Field(None)
+    cif: Optional[str] = Field(None)
+    account_number: Optional[str] = Field(None)
+    job_title: Optional[str] = Field(None)
+    organization: Optional[str] = Field(None)
+    organization_address: Optional[str] = Field(None)
+    organization_phone_number: Optional[str] = Field(None)
+    position: Optional[str] = Field(None)
+    salary_range: Optional[str] = Field(None)
+    tax_number: Optional[str] = Field(None)
+    open_biometric: Optional[bool] = Field(None)
+    avatar_image_url: Optional[str] = Field(None)
+    avatar_image_uri: Optional[str] = Field(None)
+    attachment_info: Optional[list] = Field(None)
+    finger_ids: Optional[list] = Field(None)
+    face_ids: Optional[list] = Field(None)
+    ekyc_level: Optional[str] = Field(None)
+    ekyc_step: Optional[List[EKYCStep]] = Field(None)
+    kss_status: Optional[str] = Field(None)
+    status: Optional[str] = Field(None)
+    user_eb: Optional[str] = Field(None, description="Username Ebanking")
 
     @validator('*', pre=True)
     def check_date(cls, v):
@@ -68,6 +83,12 @@ class CreateEKYCCustomerRequest(BaseSchema):
             return value
         return v
 
+    @validator('*')
+    def check_blank_str(cls, v):
+        if v == '':
+            return None
+        return v
+
 
 class CreateEKYCCustomerResponse(BaseSchema):
-    customer_id: str = Field(..., description='Mã khách hàng (ekyc)')
+    customer_id: Optional[str] = Field(..., description='Mã khách hàng (ekyc)')
