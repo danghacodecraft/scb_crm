192165abd91cfd857516fd9f946beac9f7fa8592
cuonglv4 <cuonglv4@scb.com.vn>
2022-08-09 14:01:59 +0700

    [refactor](#91): bug english

--- app/api/v1/endpoints/post_check/schema.py
+++ app/api/v1/endpoints/post_check/schema.py
@@ -140,9 +140,9 @@ class StatisticsProfilesResponse(BaseSchema):
     processing: int = Field(..., description="Chờ hậu kiểm")
     rejected: int = Field(..., description="Cần xác minh")
     failed: int = Field(..., description="Thất bại")
-    invalid: int = Field(..., description="Hợp lí")
+    valid: int = Field(..., description="Hợp lệ")
     verification: int = Field(..., description="Cần xác minh")
-    notvalid: int = Field(..., description="Không hợp lệ")
+    invalid: int = Field(..., description="Không hợp lệ")
     pending: int = Field(..., description="Chờ")
 
 
