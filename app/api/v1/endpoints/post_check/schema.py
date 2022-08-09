20da61842021449c7198824d2e774ce2eef05cbc
cuonglv4 <cuonglv4@scb.com.vn>
2022-08-09 13:51:39 +0700

    [refactor](#90): schema response

--- app/api/v1/endpoints/post_check/schema.py
+++ app/api/v1/endpoints/post_check/schema.py
@@ -140,6 +140,10 @@ class StatisticsProfilesResponse(BaseSchema):
     processing: int = Field(..., description="Chờ hậu kiểm")
     rejected: int = Field(..., description="Cần xác minh")
     failed: int = Field(..., description="Thất bại")
+    invalid: int = Field(..., description="Hợp lí")
+    verification: int = Field(..., description="Cần xác minh")
+    notvalid: int = Field(..., description="Không hợp lệ")
+    pending: int = Field(..., description="Chờ")
 
 
 ####################################################################################################
