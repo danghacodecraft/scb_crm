0d4e4f1a9940ec8f4371c1ee2917388c88b08930
aitcq <aitcq@scb.com.vn>
2022-08-10 10:58:46 +0700

    [add](#55): Product Fee Category

--- app/api/v1/endpoints/config/product/repository.py
+++ app/api/v1/endpoints/config/product/repository.py
@@ -11,9 +11,10 @@ async def repos_get_fee_category(business_type_id: str, session: Session):
     product_fee_category_info = session.execute(
         select(
             ProductFeeCategory,
-            ProductFeeBusiness
+            ProductFeeBusiness.category_id
         )
         .join(ProductFeeCategory, ProductFeeBusiness.category_id == ProductFeeCategory.id)
         .filter(ProductFeeBusiness.business_type_id == business_type_id)
+        .distinct(ProductFeeBusiness.category_id)
     ).scalars().all()
     return ReposReturn(data=product_fee_category_info)
