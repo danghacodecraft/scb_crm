08a10ae513cc56cee8de0701bb49599e9ab118fa
datnt <datnt@minerva.vn>
2022-08-12 15:35:56 +0700

    [add](#286): selectCardInfo, email template.

--- app/api/v1/endpoints/third_parties/gw/card_works/controller.py
+++ app/api/v1/endpoints/third_parties/gw/card_works/controller.py
@@ -1,8 +1,10 @@
 from app.api.base.controller import BaseController
 from app.api.v1.endpoints.third_parties.gw.card_works.repository import (
-    repos_gw_open_cards
+    repos_gw_open_cards, repos_gw_select_card_info
 )
-from app.utils.constant.gw import GW_FUNC_OPEN_CARDS_OUT
+from app.utils.constant.gw import (
+    GW_FUNC_OPEN_CARDS_OUT, GW_FUNC_SELECT_CARD_INFO_OUT
+)
 from app.utils.error_messages import ERROR_CALL_SERVICE_GW
 
 
@@ -17,5 +19,18 @@ class CtrGWCardWorks(BaseController):
             return self.response_exception(msg=ERROR_CALL_SERVICE_GW, detail=str(gw_card_works))
 
         response_data = gw_card_works[GW_FUNC_OPEN_CARDS_OUT]['data_output']
+
+        return self.response(data=response_data)
+
+    async def ctr_gw_select_card_info(self, card_branched):
+        is_success, select_card_info = self.call_repos(await repos_gw_select_card_info(
+            current_user=self.current_user.user_info,
+            card_branched=card_branched
+        ))
+
+        if not is_success:
+            return self.response_exception(msg=ERROR_CALL_SERVICE_GW, detail=str(select_card_info))
+
+        response_data = select_card_info[GW_FUNC_SELECT_CARD_INFO_OUT]['data_output']
 
         return self.response(data=response_data)
--- app/api/v1/endpoints/third_parties/gw/card_works/view.py
+++ app/api/v1/endpoints/third_parties/gw/card_works/view.py
@@ -10,7 +10,7 @@ from app.api.v1.endpoints.third_parties.gw.card_works.controller import (
     CtrGWCardWorks
 )
 from app.api.v1.endpoints.third_parties.gw.card_works.schema import (
-    OpenCardsRequest
+    OpenCardsRequest, SelectCardInfoRequest, SelectCardInfoResponse
 )
 
 router = APIRouter()
@@ -33,3 +33,22 @@ async def view_gw_open_cards(
         data=json.loads(request.json())
     )
     return ResponseData(**gw_open_cards)
+
+
+@router.post(
+    path="/select-card-info/",
+    name="[GW] Lấy thông tin nhóm thẻ, loại thẻ, chi tiết Thẻ",
+    description="[GW] Lấy thông tin nhóm thẻ, loại thẻ, chi tiết Thẻ",
+    responses=swagger_response(
+        response_model=SelectCardInfoResponse,
+        success_status_code=status.HTTP_200_OK
+    )
+)
+async def view_gw_select_card_info(
+        request: SelectCardInfoRequest = Body(...),
+        current_user=Depends(get_current_user_from_header())
+):
+    gw_select_card_info = await CtrGWCardWorks(current_user).ctr_gw_select_card_info(
+        card_branched=request.card_branched
+    )
+    return ResponseData(**gw_select_card_info)
