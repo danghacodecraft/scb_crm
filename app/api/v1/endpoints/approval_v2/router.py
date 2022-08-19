d642956b0f80ed08cf1268dcb9703453a9b60379
aitcq <aitcq@scb.com.vn>
2022-08-19 11:01:15 +0700

    [add]: approval v2 -- signature

--- app/api/v1/endpoints/approval_v2/router.py
+++ app/api/v1/endpoints/approval_v2/router.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter
 
 from app.api.v1.endpoints.approval_v2 import view as views_approval_info
 from app.api.v1.endpoints.approval_v2.face import view as views_face_info
+from app.api.v1.endpoints.approval_v2.signature import \
+    view as views_signature_info
 
 router_module = APIRouter()
 
@@ -13,3 +15,6 @@ router_module.include_router(router=views_approval_info.router, prefix="/{cif_id
 
 # router của [Thông tin xác thực] -> Khuôn mặt
 router_module.include_router(router=views_face_info.router)
+
+# router của [Thông tin xác thực] -> Chữ ký
+router_module.include_router(router=views_signature_info.router)
