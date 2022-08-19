e56a9a58bb740585cff0845a043cb71951dca260
Võ Văn Dương <duongvv@minerva.vn>
2022-08-19 16:05:57 +0700

    [add](#287): send paired message

--- app/api/v1/endpoints/tablet/mobile/controller.py
+++ app/api/v1/endpoints/tablet/mobile/controller.py
@@ -1,7 +1,10 @@
 from app.api.base.controller import BaseController
 from app.api.v1.endpoints.tablet.mobile.repository import repos_pair_by_otp
 from app.api.v1.endpoints.tablet.mobile.schema import SyncWithWebByOTPRequest
-from app.utils.constant.tablet import DEVICE_TYPE_MOBILE
+from app.settings.event import service_rabbitmq
+from app.utils.constant.tablet import (
+    DEVICE_TYPE_MOBILE, DEVICE_TYPE_WEB, WEB_ACTION_PAIRED
+)
 from app.utils.tablet_functions import (
     get_client_broker_config_info, get_topic_name
 )
@@ -25,7 +28,18 @@ class CtrTabletMobile(BaseController):
         )
         mqtt_info['topic_name'] = topic_name
 
-        # TODO: send to mqtt message
+        # gửi cho web message thông báo paired thành công
+        service_rabbitmq.publish(
+            message={
+                "action": WEB_ACTION_PAIRED,
+                "data": {}
+            },
+            routing_key=get_topic_name(
+                device_type=DEVICE_TYPE_WEB,
+                tablet_id=tablet_info['tablet_id'],
+                otp=request.otp
+            )
+        )
 
         return self.response({
             'mqtt_info': mqtt_info,
