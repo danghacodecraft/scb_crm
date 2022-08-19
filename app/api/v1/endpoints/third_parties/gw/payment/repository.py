a30eb9dbfdd7663406030edc1fe9c25f6fafea6a
lebadai <30581741+lebadai@users.noreply.github.com>
2022-08-19 15:53:58 +0700

    [refactor][amount_unblock] delete  saving_booking_account,  saving_booking_customer

--- app/api/v1/endpoints/third_parties/gw/payment/repository.py
+++ app/api/v1/endpoints/third_parties/gw/payment/repository.py
@@ -8,8 +8,8 @@ from app.api.v1.endpoints.user.schema import AuthResponse
 from app.api.v1.others.booking.repository import generate_booking_code
 from app.settings.event import service_gw
 from app.third_parties.oracle.models.cif.form.model import (
-    Booking, BookingAccount, BookingBusinessForm, BookingCustomer,
-    TransactionDaily, TransactionSender
+    Booking, BookingAccount, BookingBusinessForm, TransactionDaily,
+    TransactionSender
 )
 from app.third_parties.oracle.models.master_data.others import (
     SlaTransaction, TransactionJob, TransactionStage, TransactionStageLane,
@@ -247,8 +247,8 @@ async def repos_payment_amount_unblock(
         saving_transaction_sender: dict,
         saving_transaction_job: dict,
         saving_booking_business_form: dict,
-        saving_booking_customer,
-        saving_booking_account,
+        # saving_booking_customer,
+        # saving_booking_account,
         session
 ):
     session.add_all([
@@ -264,8 +264,8 @@ async def repos_payment_amount_unblock(
         TransactionJob(**saving_transaction_job),
         BookingBusinessForm(**saving_booking_business_form)
     ])
-    session.bulk_save_objects(BookingAccount(**account) for account in saving_booking_account)
-    session.bulk_save_objects(BookingCustomer(**customer) for customer in saving_booking_customer)
+    # session.bulk_save_objects(BookingAccount(**account) for account in saving_booking_account)
+    # session.bulk_save_objects(BookingCustomer(**customer) for customer in saving_booking_customer)
     # Update Booking
     session.execute(
         update(Booking)
