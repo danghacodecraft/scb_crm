e8547a3db62c66d3f7d0d0546f42c157dce149c9
Võ Văn Dương <duongvv@minerva.vn>
2022-08-23 11:53:00 +0700

    [add](#287): init booking authentication

--- app/api/v1/endpoints/tablet/mobile/repository.py
+++ app/api/v1/endpoints/tablet/mobile/repository.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import Session
 from starlette import status
 
 from app.api.base.repository import ReposReturn, auto_commit
+from app.third_parties.oracle.models.booking.model import BookingAuthentication
 from app.third_parties.oracle.models.tablet.model import Tablet
 from app.utils.error_messages import ERROR_TABLET_OTP_INVALID
 from app.utils.functions import now
@@ -59,3 +60,19 @@ async def repos_retrieve_table_by_tablet_token(tablet_id: str, otp: str, session
         'teller_username': tablet.teller_username,
         'otp': tablet.otp
     })
+
+
+async def repos_init_booking_authentication(tablet_id: str, teller_username: str, identity_number: str, cif_number: str,
+                                            session: Session) -> ReposReturn:
+    session.add(
+        BookingAuthentication(
+            tablet_id=tablet_id,
+            teller_username=teller_username,
+            identity_number=identity_number,
+            created_at=now(),
+            cif_number=cif_number
+        )
+    )
+    session.commit()
+
+    return ReposReturn(data=True)
