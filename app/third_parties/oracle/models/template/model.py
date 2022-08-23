01ede743894bbb5b41269b271d99e04eb2fa8553
Nguyễn Xuân Thịnh <thinhnx@Minerva.vn>
2022-08-23 11:27:02 +0700

    [add](#288): model crm_template

---
+++ app/third_parties/oracle/models/template/model.py
@@ -0,0 +1,13 @@
+from cx_Oracle import NUMBER
+from sqlalchemy import VARCHAR, Column, text
+
+from app.third_parties.oracle.base import Base
+
+
+class Tablet(Base):
+    __tablename__ = 'crm_template'
+    id = Column(VARCHAR(36), primary_key=True, server_default=text("sys_guid() "))
+    template_id = Column(NUMBER(19), comment='id của template bên TMS', nullable=False)
+    slug = Column(VARCHAR(500), comment='slug template', nullable=False)
+    url_template = Column(VARCHAR(100), comment='url biểu mẫu', nullable=False)
+    business_type_id = Column(VARCHAR(36), comment='Mã nghiệp vụ', nullable=False)
