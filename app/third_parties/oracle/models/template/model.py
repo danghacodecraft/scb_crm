35046ac83de3b5b4ea2165c60156b07a2773b3ae
Nguyễn Xuân Thịnh <thinhnx@Minerva.vn>
2022-08-23 11:30:22 +0700

    [fix](#288): change length field

--- app/third_parties/oracle/models/template/model.py
+++ app/third_parties/oracle/models/template/model.py
@@ -8,6 +8,6 @@ class Tablet(Base):
     __tablename__ = 'crm_template'
     id = Column(VARCHAR(36), primary_key=True, server_default=text("sys_guid() "))
     template_id = Column(NUMBER(19), comment='id của template bên TMS', nullable=False)
-    slug = Column(VARCHAR(500), comment='slug template', nullable=False)
-    url_template = Column(VARCHAR(100), comment='url biểu mẫu', nullable=False)
+    slug = Column(VARCHAR(300), comment='slug template', nullable=False)
+    url_template = Column(VARCHAR(500), comment='url biểu mẫu', nullable=False)
     business_type_id = Column(VARCHAR(36), comment='Mã nghiệp vụ', nullable=False)
