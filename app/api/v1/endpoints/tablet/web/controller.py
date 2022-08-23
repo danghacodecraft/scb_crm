611085ffafb33e16883c80e41b9032e7d0b0e20c
Võ Văn Dương <duongvv@minerva.vn>
2022-08-23 17:26:14 +0700

    [add](#287): return customer full name and avatar if exist

--- app/api/v1/endpoints/tablet/web/controller.py
+++ app/api/v1/endpoints/tablet/web/controller.py
@@ -1,12 +1,14 @@
 from pydantic import ValidationError
 
 from app.api.base.controller import BaseController
+from app.api.v1.endpoints.file.repository import repos_download_file
 from app.api.v1.endpoints.tablet.mobile.repository import (
     repos_retrieve_current_booking_authentication_by_tablet_id,
     repos_update_current_booking_authentication_by_tablet_id
 )
 from app.api.v1.endpoints.tablet.web.repository import (
     repos_create_tablet_otp, repos_delete_tablet_if_exists,
+    repos_get_customer_avatar_url_and_full_name_if_exist_by_booking_authentication_id,
     repos_retrieve_tablet
 )
 from app.api.v1.endpoints.tablet.web.schema import (
@@ -15,7 +17,7 @@ from app.api.v1.endpoints.tablet.web.schema import (
 )
 from app.api.v1.endpoints.user.repository import repos_get_username_from_token
 from app.api.v1.others.booking.repository import repos_check_exist_booking
-from app.settings.event import service_rabbitmq
+from app.settings.event import INIT_SERVICE, service_rabbitmq
 from app.utils.constant.tablet import (
     DEVICE_TYPE_MOBILE, DEVICE_TYPE_WEB, MOBILE_ACTION_NEW_TRANSACTION,
     MOBILE_ACTION_PROCESS_TRANSACTION, MOBILE_ACTION_SIGN,
@@ -109,11 +111,6 @@ class CtrTabletWeb(BaseController):
             except ValidationError:
                 return self.response_exception(msg='', detail='Check transaction_name & booking_id in extra_data again')
 
-            data['avatar_url'] = ''  # TODO: avatar khách hàng hiện tại
-            data['rank_type_id'] = ''  # TODO: rank id khách hàng hiện tại
-            data['rank_type_name'] = ''  # TODO: rank name khách hàng hiện tại
-            data['transaction_name'] = extra_data['transaction_name']
-
             # check exist thì đáng lý return bên repo đó luôn nhưng code các phần khác như vậy nên để lại
             booking = self.call_repos(
                 await repos_check_exist_booking(
@@ -130,10 +127,6 @@ class CtrTabletWeb(BaseController):
                     session=self.oracle_session
                 )
             )
-            if not booking_authentication.identity_front_document_file_uuid or not booking_authentication.face_file_uuid:
-                return self.response_exception(
-                    msg="INVALID_IS_IDENTIFY_CUSTOMER_STEP", detail="Customer has not taken an authentic photo"
-                )
 
             booking_authentication.booking_id = extra_data['booking_id']
 
@@ -143,10 +136,50 @@ class CtrTabletWeb(BaseController):
                     session=self.oracle_session
                 )
             )
+
+            avatar_uuid_and_full_name = self.call_repos(
+                await repos_get_customer_avatar_url_and_full_name_if_exist_by_booking_authentication_id(
+                    booking_authentication_id=booking_authentication.id,
+                    session=self.oracle_session
+                )
+            )
+            if avatar_uuid_and_full_name['avatar_uuid']:
+                avatar_url = self.call_repos(await repos_download_file(avatar_uuid_and_full_name['avatar_uuid']))
+            else:
+                avatar_url = None
+
+            data['customer_info'] = {
+                'avatar_url': f"{INIT_SERVICE['crm_app_url']}{avatar_url['file_url']}" if avatar_url else None,
+                'full_name': avatar_uuid_and_full_name['full_name'],
+                'rank_type_id': None,  # TODO: rank id khách hàng hiện tại
+                'rank_type_name': None  # TODO: rank name khách hàng hiện tại
+            }
+            data['transaction_name'] = extra_data['transaction_name']
         elif action == MOBILE_ACTION_NEW_TRANSACTION:
-            data['avatar_url'] = ''  # TODO: avatar khách hàng hiện tại
-            data['rank_type_id'] = ''  # TODO: rank id khách hàng hiện tại
-            data['rank_type_name'] = ''  # TODO: rank name khách hàng hiện tại
+            booking_authentication = self.call_repos(
+                await repos_retrieve_current_booking_authentication_by_tablet_id(
+                    tablet_id=tablet_info['tablet_id'],
+                    session=self.oracle_session
+                )
+            )
+
+            avatar_uuid_and_full_name = self.call_repos(
+                await repos_get_customer_avatar_url_and_full_name_if_exist_by_booking_authentication_id(
+                    booking_authentication_id=booking_authentication.id,
+                    session=self.oracle_session
+                )
+            )
+            if avatar_uuid_and_full_name['avatar_uuid']:
+                avatar_url = self.call_repos(await repos_download_file(avatar_uuid_and_full_name['avatar_uuid']))
+            else:
+                avatar_url = None
+
+            data['customer_info'] = {
+                'avatar_url': f"{INIT_SERVICE['crm_app_url']}{avatar_url['file_url']}" if avatar_url else None,
+                'full_name': avatar_uuid_and_full_name['full_name'],
+                'rank_type_id': None,  # TODO: rank id khách hàng hiện tại
+                'rank_type_name': None  # TODO: rank name khách hàng hiện tại
+            }
             data['transaction_name'] = None
 
         elif action == MOBILE_ACTION_SIGN:
