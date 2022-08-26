40d65ef844eac898c10fc7041dcf4acaf4ad492b
lebadai <30581741+lebadai@users.noreply.github.com>
2022-08-26 16:42:44 +0700

    [refactor][amount_unblock] get after PD

--- app/api/v1/others/booking/controller.py
+++ app/api/v1/others/booking/controller.py
@@ -225,41 +225,57 @@ class CtrBooking(BaseController):
     async def ctr_get_booking_business_form(
             self,
             booking_id: str,
-            bussines_form_id: Optional[str],
             session: Session,
             check_completed_booking: bool = True
     ):
         """
         check_completed_booking: True -> Trả lỗi nếu booking hoàn thành
         """
-        if bussines_form_id:
-            booking_business_form = session.execute(
-                select(
-                    BookingBusinessForm,
-                    Booking
-                )
-                .join(BookingBusinessForm, and_(
-                    Booking.id == BookingBusinessForm.booking_id,
-                    BookingBusinessForm.business_form_id.notilike('%_GW')
-                ))
-                .filter(
-                    Booking.id == booking_id
-                    and BookingBusinessForm.business_form_id == bussines_form_id
-                )
-            ).scalars().all()
-        else:
-            booking_business_form = session.execute(
-                select(
-                    BookingBusinessForm,
-                    Booking
-                )
-                .join(BookingBusinessForm, and_(
-                    Booking.id == BookingBusinessForm.booking_id,
-                    BookingBusinessForm.business_form_id.notilike('%_GW')
-                ))
-                .filter(Booking.id == booking_id)
-                .order_by(desc(BookingBusinessForm.created_at))
-            ).scalars().all()
+        booking_business_form = session.execute(
+            select(
+                BookingBusinessForm,
+                Booking
+            )
+            .join(BookingBusinessForm, and_(
+                Booking.id == BookingBusinessForm.booking_id,
+                BookingBusinessForm.business_form_id.notilike('%_GW')
+            ))
+            .filter(Booking.id == booking_id)
+            .order_by(desc(BookingBusinessForm.created_at))
+        ).scalars().all()
+
+        if not booking_business_form:
+            return self.response_exception(msg=ERROR_BOOKING_BUSINESS_FORM_NOT_EXIST, loc=f"booking_id: {booking_id}")
+
+        if check_completed_booking and booking_business_form[0].booking.completed_flag:
+            return self.response_exception(msg=ERROR_BOOKING_IS_COMPLETED)
+
+        return booking_business_form[0]
+
+    async def ctr_get_booking_business_form_by_bussines_form(
+            self,
+            booking_id: str,
+            bussines_form_id: str,
+            session: Session,
+            check_completed_booking: bool = True
+    ):
+        """
+        check_completed_booking: True -> Trả lỗi nếu booking hoàn thành
+        """
+        booking_business_form = session.execute(
+            select(
+                BookingBusinessForm,
+                Booking
+            )
+            .join(BookingBusinessForm, and_(
+                Booking.id == BookingBusinessForm.booking_id,
+                BookingBusinessForm.business_form_id.notilike('%_GW')
+            ))
+            .filter(
+                Booking.id == booking_id
+                and BookingBusinessForm.business_form_id == bussines_form_id
+            )
+        ).scalars().all()
 
         if not booking_business_form:
             return self.response_exception(msg=ERROR_BOOKING_BUSINESS_FORM_NOT_EXIST, loc=f"booking_id: {booking_id}")
