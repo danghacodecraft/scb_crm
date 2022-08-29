803c297f3c99341afcbba5b218c79fbeb9333671
datnt <datnt@minerva.vn>
2022-08-29 16:43:51 +0700

    [fix](#datnt): gw subcard is None

--- app/api/v1/endpoints/cif/debit_card/repository.py
+++ app/api/v1/endpoints/cif/debit_card/repository.py
@@ -150,8 +150,8 @@ async def repos_debit_card(cif_id: str, session: Session) -> ReposReturn:
                     "number_part_4": item.card_num_mask[12:16]
                 } if item.card_num_mask and item.approval_status and len(item.card_num_mask) == 16 else {
                     "number_part_1": "123",
-                    "number_part_2": "456",
-                    "number_part_3": "789",
+                    "number_part_2": "XXX",
+                    "number_part_3": "X89",
                     "number_part_4": "111"
                 },
                 "card_image_url": "https://vi.wikipedia.org/wiki/Trang_Ch%C3%ADn"
@@ -209,8 +209,8 @@ async def repos_debit_card(cif_id: str, session: Session) -> ReposReturn:
                     "number_part_4": item.card_num_mask[12:16],
                 } if item.card_num_mask and item.approval_status and len(item.card_num_mask) == 16 else {
                     "number_part_1": "999",
-                    "number_part_2": "222",
-                    "number_part_3": "789",
+                    "number_part_2": "XXX",
+                    "number_part_3": "X44",
                     "number_part_4": "333"
                 },
                 "card_image_url": "https://vi.wikipedia.org/wiki/Trang_Ch%C3%ADn"  # TODO
