ab4afbc25836580d679ab74fac3fd5a76f1ac3ec
lebadai <30581741+lebadai@users.noreply.github.com>
2022-08-29 15:41:58 +0700

    [refactor][amount_unblock] fix statement

--- app/api/v1/endpoints/third_parties/gw/payment/controller.py
+++ app/api/v1/endpoints/third_parties/gw/payment/controller.py
@@ -278,7 +278,8 @@ class CtrGWPayment(CtrGWCasaAccount, CtrAccountFee):
         account_unlock_info = []
         account_numbers = []
         saving_booking_account = []
-        statement = request.transaction_fee_info.statement
+        statement_detail = request.transaction_fee_info.statement_info.statement_detail
+        statement = request.transaction_fee_info.statement_info
         management = request.transaction_fee_info.management_info
         sender_info = request.transaction_fee_info.sender_info
         cif_number = None
@@ -291,7 +292,7 @@ class CtrGWPayment(CtrGWCasaAccount, CtrAccountFee):
                 str(int(item.denominations)): 0
             })
         denominations_errors = []
-        for index, row in enumerate(statement):
+        for index, row in enumerate(statement_detail):
             denominations = row.denominations
             if denominations not in denominations__amounts:
                 denominations_errors.append(dict(
